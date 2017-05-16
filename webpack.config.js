130838db4baff7e3d503c042a82a0ec5ea731809
Denis <000scorpions000@gmail.com>
2017-05-16 22:54:36 +0300

    'test'

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-console.log(555667);
+console.log(5575667);
 
 const path = require('path');
 const webpack = require('webpack'); // ставиться локально для того чтоб вытаскивать плагины и доп. инструменты
