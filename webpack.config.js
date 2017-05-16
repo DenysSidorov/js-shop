075da8ac7b76b4c4ad90377e67c0d6d3ac9d20c4
Denis <000scorpions000@gmail.com>
2017-05-16 22:43:01 +0300

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-console.log(5556);
+console.log(55566);
 
 const path = require('path');
 const webpack = require('webpack'); // ставиться локально для того чтоб вытаскивать плагины и доп. инструменты
