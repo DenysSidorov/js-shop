b4cf7773eadc2402655823ffac7078796ecadb97
Denis <000scorpions000@gmail.com>
2017-05-24 00:05:53 +0300

    fix jquery plugin

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-import a from './components/test';
+window.$ = jQuery;
+window.jQuery = jQuery;
 
+import a from './components/test';
+// import $ from 'jquery';
+// window.$ = $;
 console.log(a,'33wee22r4r33');
-var af = 352;
+var af = 35222;
