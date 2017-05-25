01b21b6a1a0d84f20b939872b24e53398e137932
Denis <000scorpions000@gmail.com>
2017-05-25 22:36:19 +0300

    clean app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,4 @@ window.$ = jQuery;
 window.jQuery = jQuery;
 
 import a from './components/test';
-// import $ from 'jquery';
-// window.$ = $;
-console.log(a,'33wee22r4r33');
-var af = 35222;
+console.log('test');
