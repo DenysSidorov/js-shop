bb97195143406eaca048e72dac08204fbea04e6f
Denis <000scorpions000@gmail.com>
2017-05-29 17:16:18 +0300

    add build tools

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 module.exports = {
     plugins: {
+        'postcss-smart-import': {},
+        //'precss':{/* ...options */},
         'autoprefixer': {},
 
+
+        //  испольвание нового синтаксиса в css        http://cssnext.io/
+          'cssnext': {/* ...options */},
+        //  проверка синтаксиса, оптимизация  и т.д.   http://cssnano.co/
+        // 'cssnano': {/* ...options */},
     }
 
 };
