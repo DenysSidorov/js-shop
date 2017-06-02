bfcae72419aafb6cf7c44a24a0bbf47137564613
Denis <000scorpions000@gmail.com>
2017-06-02 14:17:23 +0300

    add cart with dataset

--- www/js/function.js
+++ www/js/function.js
@@ -1,29 +1,29 @@
-$(document).ready(function () {
-    if ($('.navBurger').length > 0) {
-        $('.navBurger').on('click', function (e) {
-            e.preventDefault();
-            $('body').toggleClass('nonScroll');
-            $(this).toggleClass('active')
-                .parents('.headerSection')
-                .find('.headerNavigation ul')
-                .toggleClass('active');
-        });
-    }
-
-
-
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        var parallaxBlock = $('.contentGoodsSection').offset().top;
-        var i = 0;
-        if (scroll >= parallaxBlock - 200) {
-            $(".parallaxService .serviceItem").each(function () {
-                var block = $(this);
-                i += 500;
-                setTimeout(function () {
-                    block.addClass('active');
-                }, i);
-            });
-        }
-    })
-});
+// $(document).ready(function () {
+//     if ($('.navBurger').length > 0) {
+//         $('.navBurger').on('click', function (e) {
+//             e.preventDefault();
+//             $('body').toggleClass('nonScroll');
+//             $(this).toggleClass('active')
+//                 .parents('.headerSection')
+//                 .find('.headerNavigation ul')
+//                 .toggleClass('active');
+//         });
+//     }
+//
+//
+//
+//     $(window).scroll(function () {
+//         var scroll = $(window).scrollTop();
+//         var parallaxBlock = $('.contentGoodsSection').offset().top;
+//         var i = 0;
+//         if (scroll >= parallaxBlock - 200) {
+//             $(".parallaxService .serviceItem").each(function () {
+//                 var block = $(this);
+//                 i += 500;
+//                 setTimeout(function () {
+//                     block.addClass('active');
+//                 }, i);
+//             });
+//         }
+//     })
+// });
