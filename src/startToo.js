e6101d6579f88c98b677b8e0792453e7ef06970f
Denis <000scorpions000@gmail.com>
2017-07-16 22:32:46 +0300

    fix in slider

--- src/startToo.js
+++ src/startToo.js
@@ -9,15 +9,15 @@ var slickContainerSimilarGoods = $('.slickContainerSimilarGoods');
 if(slickContainerSimilarGoods.length) {
     $('.slickContainerSimilarGoods').slick({ // -----------------------slick slider #2---------------
         //dots: true,
-        infinite: false,
+        infinite: true,
         speed: 300,
         autoplay: true,
         autoplaySpeed: 2000,
-        slidesToShow: 4,
+        slidesToShow: 5,
         slidesToScroll: 2,
         responsive: [
             {
-                breakpoint: 1024,
+                //breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
