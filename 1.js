a27a60bd81804937f5b85b2181245da64cf66081
Denis <000scorpions000@gmail.com>
2017-07-28 00:48:16 +0300

    add yarn, eslint

---
+++ 1.js
@@ -0,0 +1,11 @@
+function f( ){
+
+}
+
+var a = 6;
+
+var fd = {
+    v : g => {
+
+    }
+};
---
+++ serverK.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Denis on 7/27/2017.
+ */
+// npm i --save-dev eslint - проверка кода
+// npm i --save-dev eslint-config-airbnb-base - конфиг для eslint (с react)
+// eslint-plugin-import  - дополнительная зависимость чтоб небыло конфликтов, т.к при уст. был warning
+// npm i --save-dev eslint-config-prettier   отключает сторонние конфиги и правила, применяя только основные
+
