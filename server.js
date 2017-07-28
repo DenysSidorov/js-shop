ea8115bdc37882d7830e00898dbdf1c1de399601
Denis <000scorpions000@gmail.com>
2017-07-28 17:22:29 +0300

    test server async/await

--- server.js
+++ server.js
@@ -1,61 +1,94 @@
-import path from 'path';
-const S = path.resolve(__dirname, './app-server');
-import express from 'express';
-import mongoose from 'mongoose'; // MongoDb ORM
-import session from 'express-session';
-import bodyParse from 'body-parser';
-import morgan from 'morgan'; //  Логирование
+import http from 'http';
 
-// TODO download CORS-middleware and require it here
+console.log('START TEST');
 
-import config from './config/index';
-// import config from './app-server/config'; // Конфигурация
-import authRoute from './app-server/routes/auth';
-import userRoute from './app-server/routes/user';
-import pageRoute from './app-server/routes/page';
-import errorMiddleWare from './app-server/middlewares/errors';
-
-import getUser from './app-server/middlewares/getUser'; // Проверка налисия токена
-import checkToken from './app-server/middlewares/checkToken'; // Проверка налисия токена
+async function b(a) {
+   var pr = new Promise(function(resolve, reject) {
 
 
-const app = express(); // Запуск приложения
 
-/** Подключение к базе данных mongodb*/
-mongoose.Promise = require('bluebird'); // Для асинхронного кода
-mongoose.connect(config.backend.database, {}, err => {
-    if (err) throw err;
-    console.log(`Mongo connected!`);
-});
+       var res = 3;
+       setTimeout( ()=>{
+           resolve(a * a);
+       }, 1000);
 
-/** Запуск приожения на порте*/
-app.listen(config.backend.port, (err)=>{
-    if (err) throw err;
-    console.log('Server listening on port ' + config.backend.port );
-});
-
-app.use(morgan('tiny')); // Настройка логирования, см. документация на npmjs.com
-
-app.use(bodyParse.json());
-app.use(bodyParse.urlencoded({extend: true}));
-
-/*TODO make async function*/ app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: config.backend.secretWord
-}));
-
-app.use('/api', authRoute); // singin singup
-
-app.use('/api', checkToken,  userRoute); // get user route
-app.use(getUser);
-app.use('/api', checkToken,  pageRoute); //
-app.get('/test', checkToken, (req, resp)=>{ // check token in headers
-    resp.json('Success');
-});
+   });
 
 
-app.use(errorMiddleWare ); // Обработчик ошибок должен быть последним
+    return pr;
+}
+
+
+async function d(){
+    var k =1 ;
+    try {
+        k = await b(100);
+    } catch (e){
+        console.log('errro');
+    }
+
+    console.log(k, 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
+}
+d();
+
+// import path from 'path';
+// const S = path.resolve(__dirname, './app-server');
+// import express from 'express';
+// import mongoose from 'mongoose'; // MongoDb ORM
+// import session from 'express-session';
+// import bodyParse from 'body-parser';
+// import morgan from 'morgan'; //  Логирование
+//
+// // TODO download CORS-middleware and require it here
+//
+// import config from './config/index';
+// // import config from './app-server/config'; // Конфигурация
+// import authRoute from './app-server/routes/auth';
+// import userRoute from './app-server/routes/user';
+// import pageRoute from './app-server/routes/page';
+// import errorMiddleWare from './app-server/middlewares/errors';
+//
+// import getUser from './app-server/middlewares/getUser'; // Проверка налисия токена
+// import checkToken from './app-server/middlewares/checkToken'; // Проверка налисия токена
+//
+//
+// const app = express(); // Запуск приложения
+//
+// /** Подключение к базе данных mongodb*/
+// mongoose.Promise = require('bluebird'); // Для асинхронного кода
+// mongoose.connect(config.backend.database, {}, err => {
+//     if (err) throw err;
+//     console.log(`Mongo connected!`);
+// });
+//
+// /** Запуск приожения на порте*/
+// app.listen(config.backend.port, (err)=>{
+//     if (err) throw err;
+//     console.log('Server listening on port ' + config.backend.port );
+// });
+//
+// app.use(morgan('tiny')); // Настройка логирования, см. документация на npmjs.com
+//
+// app.use(bodyParse.json());
+// app.use(bodyParse.urlencoded({extend: true}));
+//
+// /*TODO make async function*/ app.use(session({
+//     resave: true,
+//     saveUninitialized: true,
+//     secret: config.backend.secretWord
+// }));
+//
+// app.use('/api', authRoute); // singin singup
+//
+// app.use('/api', checkToken,  userRoute); // get user route
+// app.use(getUser);
+// app.use('/api', checkToken,  pageRoute); //
+// app.get('/test', checkToken, (req, resp)=>{ // check token in headers
+//     resp.json('Success');
+// });
+//
+//
+// app.use(errorMiddleWare ); // Обработчик ошибок должен быть последним
 
 
 
--- serverK.js
+++ serverK.js
@@ -1,8 +1,11 @@
-/**
- * Created by Denis on 7/27/2017.
- */
 // npm i --save-dev eslint - проверка кода
-// npm i --save-dev eslint-config-airbnb-base - конфиг для eslint (с react)
+// npm i --save-dev eslint-config-airbnb - конфиг для eslint (с react)
+// eslint-plugin-jsx-a11y   -  для поддержки jsx
+// eslint-plugin-react - для поддержки react
 // eslint-plugin-import  - дополнительная зависимость чтоб небыло конфликтов, т.к при уст. был warning
 // npm i --save-dev eslint-config-prettier   отключает сторонние конфиги и правила, применяя только основные
 
+
+// "eslintConfig": {
+//     "extends": "airbnb"
+// }
