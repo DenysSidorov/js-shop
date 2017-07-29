12a5780093a95ef5f4be0fca1927d93f5194dd4e
Denis <000scorpions000@gmail.com>
2017-07-29 23:44:03 +0300

    replays eslint

--- webpack.config.js
+++ webpack.config.js
@@ -204,3 +204,21 @@ if (true) {
 }
 
 module.exports = config;
+
+
+
+
+
+// Настройка для Node EsLint
+
+// npm i --save-dev eslint - проверка кода
+// npm i --save-dev eslint-config-airbnb - конфиг для eslint (с react)
+// eslint-plugin-jsx-a11y   -  для поддержки jsx
+// eslint-plugin-react - для поддержки react
+// eslint-plugin-import  - дополнительная зависимость чтоб небыло конфликтов, т.к при уст. был warning
+// npm i --save-dev eslint-config-prettier   отключает сторонние конфиги и правила, применяя только основные
+
+
+// "eslintConfig": {
+//     "extends": "airbnb"
+// }
