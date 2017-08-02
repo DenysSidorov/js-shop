3cd8f7e84e55cc484443567d78d91c24b4aa43d4
DenysSidorov <000scorpions000@gmail.com>
2017-08-03 00:13:06 +0300

    helper async-await

---
+++ app-server/helper/async-await-test.js
@@ -0,0 +1,26 @@
+console.log('START TEST');
+
+async function b(a) {
+   var pr = new Promise(function(resolve, reject) {
+
+       var res = 3;
+       setTimeout( ()=>{
+           resolve(a * a);
+       }, 1000);
+
+   });
+    return pr;
+}
+
+
+async function d(){
+    var k =1 ;
+    try {
+        k = await b(100);
+    } catch (e){
+        console.log('errro');
+    }
+
+    console.log(k, 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
+}
+d();
