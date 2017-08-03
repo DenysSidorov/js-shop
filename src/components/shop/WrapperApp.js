f37f92d864808f6528f64de62ecbc76be10b7208
DenysSidorov <000scorpions000@gmail.com>
2017-08-03 23:28:04 +0300

    add MainBlock Component

--- src/components/shop/WrapperApp.js
+++ src/components/shop/WrapperApp.js
@@ -3,31 +3,25 @@ import MainMenu from "./MainMenu";
 import ConfirmBlock from "./ConfirmBlock";
 import BtnUp from "./BtnUp";
 import FooterSection from "./FooterSection";
+import MainContent from "./MainContent";
 
 class WrapperApp extends React.Component {
     render() {
         return (
-            <div>
                 <div className="wrapper">
                     <div className="content">
-                        <MainMenu/>
                         {/*<ConfirmBlock/>*/}
-
-                        <div className="mainContainerSection fullWidth left">
-                            <div className="container">
-                                <div className="preloader-main-content"></div>
-
-
-
-
-                            </div>
-                        </div>
-
-                        <BtnUp/>
-                        <FooterSection/>
+                        <MainMenu/>
+                        <MainContent>
+                           <h1>test 23423423 4234234 234 234 234 23432</h1>
+                        </MainContent>
                     </div>
+
+                        <FooterSection/>
+                        <BtnUp/>
+
                 </div>
-            </div>
+
 
         )
 
