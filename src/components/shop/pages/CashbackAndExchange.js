69b230736294b3e75d1f618892101da98d86585a
DenysSidorov <000scorpions000@gmail.com>
2017-08-06 00:00:58 +0300

    add CashbackAndExchange component

---
+++ src/components/shop/pages/CashbackAndExchange.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+class CashbackAndExchange extends React.Component {
+    render() {
+        return (
+            <div><br/><br/><br/><h2>CashbackAndExchange  THERE </h2> <br/><br/><br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi culpa cumque deleniti earum nam
+                natus non nostrum, officiis rem ullam vero. Debitis eveniet iste molestiae nemo quibusdam, ratione vel.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, adipisci cum dolore dolorum
+                earum, eos excepturi expedita harum laudantium magni minus nemo nisi perferendis recusandae repudiandae
+                sapiente suscipit tempore! <br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, asperiores autem
+                doloremque doloribus dolorum, eaque excepturi ipsam iure modi neque quas quis saepe sed similique sint
+                totam. Aliquid, excepturi!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi culpa cumque deleniti earum nam
+                natus non nostrum, officiis rem ullam vero. Debitis eveniet iste molestiae nemo quibusdam, ratione vel.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, adipisci cum dolore dolorum
+                earum, eos excepturi expedita harum laudantium magni minus nemo nisi perferendis recusandae repudiandae
+                sapiente suscipit tempore! <br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, asperiores autem
+                doloremque doloribus dolorum, eaque excepturi ipsam iure modi neque quas quis saepe sed similique sint
+                totam. Aliquid, excepturi!<br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi culpa cumque deleniti earum nam
+                natus non nostrum, officiis rem ullam vero. Debitis eveniet iste molestiae nemo quibusdam, ratione vel.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, adipisci cum dolore dolorum
+                earum, eos excepturi expedita harum laudantium magni minus nemo nisi perferendis recusandae repudiandae
+                sapiente suscipit tempore!<br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, asperiores autem
+                doloremque doloribus dolorum, eaque excepturi ipsam iure modi neque quas quis saepe sed similique sint
+                totam. Aliquid, excepturi! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi culpa cumque deleniti earum nam
+                natus non nostrum, officiis rem ullam vero. Debitis eveniet iste molestiae nemo quibusdam, ratione vel.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, adipisci cum dolore dolorum
+                earum, eos excepturi expedita harum laudantium magni minus nemo nisi perferendis recusandae repudiandae
+                sapiente suscipit tempore!<br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, asperiores autem
+                doloremque doloribus dolorum, eaque excepturi ipsam iure modi neque quas quis saepe sed similique sint
+                totam. Aliquid, excepturi!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi culpa cumque deleniti earum nam
+                natus non nostrum, officiis rem ullam vero. Debitis eveniet iste molestiae nemo quibusdam, ratione vel.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, adipisci cum dolore dolorum
+                earum, eos excepturi expedita harum laudantium magni minus nemo nisi perferendis recusandae repudiandae
+                sapiente suscipit tempore!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, asperiores autem
+                doloremque doloribus dolorum, eaque excepturi ipsam iure modi neque quas quis saepe sed similique sint
+                totam. Aliquid, excepturi!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi culpa cumque deleniti earum nam
+                natus non nostrum, officiis rem ullam vero. Debitis eveniet iste molestiae nemo quibusdam, ratione vel.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus, adipisci cum dolore dolorum
+                earum, eos excepturi expedita harum laudantium magni minus nemo nisi perferendis recusandae repudiandae
+                sapiente suscipit tempore!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, asperiores autem
+                doloremque doloribus dolorum, eaque excepturi ipsam iure modi neque quas quis saepe sed similique sint
+                totam. Aliquid, excepturi!
+            </div>
+        )
+
+    }
+}
+
+export default CashbackAndExchange ;
