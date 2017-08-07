1ab7cdaea82b3e2792cfb5a3f2dfb47263d73f86
DenysSidorov <000scorpions000@gmail.com>
2017-08-07 13:54:41 +0300

    add plans in wrapper getTime, isAuth, getCurrency, getName, getDate, getLocation

--- src/components/shop/index.js
+++ src/components/shop/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import WrapperApp from './WrapperApp';
 
 class App extends React.Component{
+    componentDidMount(){
+        // TODO getTime, isAuth, getCurrency, getName, getDate, getLocation, getSomeData
+    }
     render(){
+
         return (
                 <div style={{width: '100%', height: '100%'}}>
                     <WrapperApp {...this.props}/>
