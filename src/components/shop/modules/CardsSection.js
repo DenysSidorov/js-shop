a287677d925c9803dfc6c0a17a01c478d7996a73
DenysSidorov <000scorpions000@gmail.com>
2017-08-10 13:54:13 +0300

    prepare popular for similar component

--- src/components/shop/modules/CardsSection.js
+++ src/components/shop/modules/CardsSection.js
@@ -2,19 +2,20 @@ import React from "react";
 import axios from "axios";
 import CardMainPage from './CardMainPage';
 class CardsSection extends React.Component {
-    state = {cards : []};
-    async componentDidMount() {
-        // TODO getTime, isAuth, getCurrency, getName, getDate, getLocation, getSomeData
-        // https://www.npmjs.com/package/axios
-        var cards = await axios.get('http://localhost:3000/goods');
-
-        // setTimeout(()=>{this.setState({cards: cards.goods})}, 2000)
-        this.setState({cards: cards.data})
-
-         console.log(cards.data, 't');
-    }
+    // state = {cards : []};
+    // async componentDidMount() {
+    //     // TODO getTime, isAuth, getCurrency, getName, getDate, getLocation, getSomeData
+    //     // https://www.npmjs.com/package/axios
+    //     var cards = await axios.get('http://localhost:3000/goods');
+    //
+    //     // setTimeout(()=>{this.setState({cards: cards.goods})}, 2000)
+    //     this.setState({cards: cards.data})
+    //
+    //      console.log(cards.data, 't');
+    // }
 
     render() {
+        let {cards} = this.props;
         return (
             <div className="cardSection left fullWidth ">
                 <div className="container">
@@ -22,7 +23,7 @@ class CardsSection extends React.Component {
                         <div className="productsCardBlock__navigation"></div>
                         <div className="productsCardBlock__searchCard"></div>
                         <div className="productsCardBlock__bodyCardItems">
-                            { this.state.cards.map(el=> <CardMainPage card={el} key={el._id}/>)}
+                            { cards.map(el=> <CardMainPage card={el} key={el._id}/>)}
                         </div>
                     </div>
                 </div>
--- src/components/shop/pages/Home.js
+++ src/components/shop/pages/Home.js
@@ -1,21 +1,46 @@
 import React from "react";
-import CardsSection from '../modules/CardsSection';
-import TagsMainSection from '../modules/TagsMainSection';
-import MenuInfoSection from '../modules/MenuInfoSection';
-import AdditionalSection from '../modules/AdditionalSection';
-import SimilarGoodsSection from '../modules/SimilarGoodsSection';
+import axios from "axios";
+import CardsSection from "../modules/CardsSection";
+import TagsMainSection from "../modules/TagsMainSection";
+import MenuInfoSection from "../modules/MenuInfoSection";
+import AdditionalSection from "../modules/AdditionalSection";
+import SimilarGoodsSection from "../modules/SimilarGoodsSection";
 class Home extends React.Component {
-    componentDidMount(prevProps) {
+    state = {cards: []};
+
+    async componentDidMount(prevProps) {
         window.scrollTo(0, 0)
+        // TODO getTime, isAuth, getCurrency, getName, getDate, getLocation, getSomeData
+
+        // https://www.npmjs.com/package/axios
+        var cards = [];
+        var popularCards = [];
+        try {
+             cards = await axios.get('http://localhost:3000/goods');
+             popularCards = await axios.get('http://localhost:3000/goods/popular');
+            // setTimeout(()=>{this.setState({cards: cards.goods})}, 2000)
+        } catch(e){
+            console.log(e);
+
+        } finally {
+            this.setState({
+                cards: cards.data,
+                popularCards: popularCards.data
+            })
+        }
+
+        console.log(cards.data, 't');
+        console.log(popularCards.data, 'p');
     }
+
     render() {
         return (
             <div>
                 <MenuInfoSection/>
                 <TagsMainSection/>
-                <CardsSection/>
+                <CardsSection cards={this.state.cards}/>
                 <AdditionalSection/>
-                <SimilarGoodsSection title={'Популярные'}/>
+                <SimilarGoodsSection cards={this.state.popularCards} title={'Популярные'}/>
             </div>
         )
     }
