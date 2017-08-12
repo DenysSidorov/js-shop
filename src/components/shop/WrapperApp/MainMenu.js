38073155baa4f4a8d9a7c320893b5c0c481c651f
DenysSidorov <000scorpions000@gmail.com>
2017-08-12 08:56:56 +0300

    add count of goods in main menu sign

--- src/components/shop/WrapperApp/MainMenu.js
+++ src/components/shop/WrapperApp/MainMenu.js
@@ -3,6 +3,9 @@ import {Link, NavLink} from 'react-router-dom';
 import { connect } from 'react-redux';
 class MainMenu extends React.Component {
     render() {
+        let {cart} = this.props;
+        let count = 0;
+         cart.forEach(el=> {count = count + el.count);
         return (
             <div>
                 <div className="menuSection left fullWidth js_search-height">
@@ -27,7 +30,7 @@ class MainMenu extends React.Component {
                                     <li className="menu__menuHeader_item">
                                         {/*<a href="">Возврат и обмен</a>*/}
                                         <NavLink to="/cashback-and-exchange" activeStyle={{color: '#2EA9FD'}}>
-                                            Возврат и обмен</NavLink>
+                                            Возврат и обмен{count}</NavLink>
                                     </li>
                                     <li className="menu__menuHeader_item">
                                         <NavLink to="/blog" activeStyle={{color: '#2EA9FD'}}>Блог</NavLink>
@@ -37,7 +40,7 @@ class MainMenu extends React.Component {
                                     </li>
                                 </ul>
                                 <span className="main-cart">
-                                    <i className="fa fa-shopping-cart main-cart__ico" data-js_count="5"></i>
+                                    <i className="fa fa-shopping-cart main-cart__ico" data-js_count={count}></i>
                                 </span>
                             </div>
                         </div>
@@ -50,4 +53,15 @@ class MainMenu extends React.Component {
     }
 }
 
-export default MainMenu;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        cart: state.cart.items
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {return {}}
+
+export default connect(
+    mapStateToProps,
+
+)(MainMenu);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import counter from './counterRRR'
-import cart from './cart'
+import cartReducer from './cart'
 
 export default combineReducers({
     router: routerReducer, // second place react-router-redux including, first - applyMiddleware
     counter,
-    cart
+    cart: cartReducer
 })
