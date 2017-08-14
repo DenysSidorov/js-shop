992246aa6fea8a14f2a94ca76ff93887579dfa98
DenysSidorov <000scorpions000@gmail.com>
2017-08-14 15:35:27 +0300

    fix in sail formula

--- src/components/shop/pages/order/AllPrice.js
+++ src/components/shop/pages/order/AllPrice.js
@@ -5,13 +5,13 @@ class AllPrice extends React.Component {
 
     render() {
         let goods = this.props.cart;
-        let price = goods.reduce((prev, cur)=> prev + (((cur.price/100)*(100-cur.sail))*cur.count), 0);
+        let price = goods.reduce((prev, cur)=> prev + Math.floor((((cur.price/100)*(100-cur.sail))*cur.count)), 0);
         return(
             <div className="allPriceForItems">
                 <span className="allPriceForItems_text">ВСЕГО К ОПЛАТЕ:&nbsp;&nbsp;</span>
                 <span className="allPriceForItems_price">{price}</span>
                 <span className="allPriceForItems_text">&nbsp;грн.</span>
-                    <div>qweqw</div>
+                    <div></div>
             </div>
         )
 
