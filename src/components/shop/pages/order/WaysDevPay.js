91ecf84b2521844f62307dfb68f5e31638729255
DenysSidorov <000scorpions000@gmail.com>
2017-08-15 14:51:24 +0300

    validate, prepare info order

--- src/components/shop/pages/order/WaysDevPay.js
+++ src/components/shop/pages/order/WaysDevPay.js
@@ -1,28 +1,86 @@
 import React from "react";
 import {connect} from "react-redux";
-
+import Dropdown from "react-dropdown";
+// http://fraserxu.me/react-dropdown/
 class WaysDevPay extends React.Component {
     state = {
-        delivery: null,
-        payment: null,
+        payment: {value: 'predo', label: 'Приват24 Предоплата'},
+        delivery: 'newpost',
         name: '',
         phone: '',
         address: '',
-        email: ''
+        email: '',
+        isNormal: false,
     };
 
     chName(e) {
-        console.log(e);
         console.log(e.target.value);
         if (e.target.value.length < 70) {
             this.setState({name: e.target.value})
         }
+        this.validateData();
     }
 
-    componentDidMount() {
+    chPhone(e) {
+        console.log(e.target.value);
+        if (e.target.value.length < 70) {
+            this.setState({phone: e.target.value})
+        }
+        this.validateData();
+    }
+
+    chAddress(e) {
+        console.log(e.target.value);
+        if (e.target.value.length < 70) {
+            this.setState({address: e.target.value})
+        }
+        this.validateData();
+    }
+
+    chEmail(e) {
+        console.log(e.target.value);
+        if (e.target.value.length < 70) {
+            this.setState({email: e.target.value})
+        }
+    }
+
+    validateData() {
+        // your validate logic
+        var emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (this.state.name.length < 5
+            || this.state.address.length < 4
+            || this.state.phone.length < 4
+        ) {
+            this.setState({
+                isNormal: false
+            })
+        } else {
+            this.setState({
+                isNormal: true
+            })
+        }
+    }
+
+    chPayment(paymentObj) {
+        this.setState({payment: paymentObj});
+    }
+
+    dispatchData() {
+            console.log(this.state);
+    }
+
+    chDelivery(kind, e) {
+        console.log(kind, 'k');
+        this.setState({delivery: kind})
     }
 
     render() {
+        const payment = [
+            {value: 'predo', label: 'Приват24 Предоплата'},
+            {value: 'naloj', label: 'Наложенный платеж'}
+        ]
+
         return (
             <div className="userWaysContainer">
                 <div className="userWays">
@@ -30,10 +88,15 @@ class WaysDevPay extends React.Component {
 
                     <ul className="accordion">
                         <li className="accordion_item">
-                            <input type="radio" className="accordion_toggle" name="accordion-01" id="toggle-01"
-                                   readOnly checked/>
-                            <label className="accordion_trigger" htmlFor="toggle-01">Доставка по Украине "Новая
-                                Почта"</label>
+                            <input type="radio"
+                                   className="accordion_toggle"
+                                   name="accordion-01"
+                                   id="toggle-01"
+                                   readOnly
+                                   checked/>
+                            <label  onClick={this.chDelivery.bind(this, 'newpost')} className="accordion_trigger"
+                                   htmlFor="toggle-01">Доставка по Украине "Новая Почта"
+                            </label>
                             <label htmlFor="toggle-01"></label>
                             <div className="accordion_target">
                                 <p className="">
@@ -43,10 +106,10 @@ class WaysDevPay extends React.Component {
                                 </p>
                             </div>
                         </li>
-                        <li className="accordion_item">
+                        <li  className="accordion_item">
                             <input type="radio" className="accordion_toggle" name="accordion-01"
                                    id="toggle-02"/>
-                            <label className="accordion_trigger" htmlFor="toggle-02">Доставка по Украине
+                            <label onClick={this.chDelivery.bind(this, 'intime')} className="accordion_trigger" htmlFor="toggle-02">Доставка по Украине
                                 "Интайм"</label>
                             <div className="accordion_target">
                                 <p>
@@ -58,10 +121,15 @@ class WaysDevPay extends React.Component {
 
                     <div className="tittleWAyName" data-count="2">Способ оплаты</div>
                     <div className="paymentWayOrder">
-                        <select>
-                            <option>Пункт 1</option>
-                            <option>Пункт 2</option>
-                        </select>
+                        <Dropdown
+                            options={payment}
+                            onChange={this.chPayment.bind(this)}
+                            value={this.state.payment.label}
+                        />
+                        {/*<select>*/}
+                        {/*<option>Пункт 1</option>*/}
+                        {/*<option>Пункт 2</option>*/}
+                        {/*</select>*/}
                     </div>
 
                     <div className="tittleWAyName" data-count="3">Способ доставки</div>
@@ -80,8 +148,13 @@ class WaysDevPay extends React.Component {
                         <input value={this.state.email} onChange={this.chEmail.bind(this)}
                                type="text" className="orderWaysInput" id="mailInput"/>
                     </div>
+                    {this.state.isNormal
+                        ? <div onClick={this.dispatchData.bind(this)} className="orderWaysConfirmBtn">
+                        <span>Подтвердить</span></div>
+                        : <div className="orderWaysConfirmBtnDsbl"><span>Подтвердить</span></div>
+                    }
 
-                    <div className="orderWaysConfirmBtn"><span>Подтвердить</span></div>
+
                 </div>
             </div>
         )
