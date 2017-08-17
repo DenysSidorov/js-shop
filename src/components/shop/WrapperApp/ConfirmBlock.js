5de28fd37309f13abf0838953078bc9a20e0768b
DenysSidorov <000scorpions000@gmail.com>
2017-08-17 22:51:50 +0300

    add cancel, ok handlers in confirm component

--- src/components/shop/WrapperApp/ConfirmBlock.js
+++ src/components/shop/WrapperApp/ConfirmBlock.js
@@ -1,6 +1,15 @@
 import React from "react";
 import st from './confirmBlock.less';
 class ConfirmBlock extends React.Component {
+
+
+    okHandler(){
+        this.props.okHandler()
+    }
+    cancelHandler(){
+        this.props.cancelHandler()
+    }
+
     render() {
         return (
             <div className="confirmBlockForUser">
@@ -8,33 +17,10 @@ class ConfirmBlock extends React.Component {
         <span className="confirmBlockForUser__content_btn_close">
         <i className="fa fa-times" aria-hidden="true"></i>
         </span>
-                    <ul>
-                        <li>
-                            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad amet beatae,
-                                debitis doloremque dolores ducimus facilis id laboriosam laudantium maiores mollitia,
-                                nemo
-                                nostrum numquam, quos suscipit vel velit voluptatem.
-                            </div>
-                            <div>Debitis dolorem odit optio suscipit. Ad animi asperiores assumenda beatae corporis
-                                culpa
-                                cumque debitis eos explicabo facere inventore laboriosam, maxime nihil quae quasi
-                                ratione
-                                repellat sint temporibus totam vitae voluptas!
-                            </div>
-                            <div>Assumenda cupiditate dolores earum eius facere, fugit maiores quasi, qui recusandae
-                                reiciendis rem repellendus repudiandae suscipit. Alias amet animi consectetur
-                                dignissimos
-                                excepturi incidunt ipsam, ipsum modi nulla porro saepe sapiente.
-                            </div>
-                            <div>Ab asperiores aut cupiditate, ducimus, eos facilis nemo non obcaecati quasi quidem quis
-                                tenetur vel, vero. Consequuntur, dolorum incidunt ipsum numquam possimus provident quas.
-                                Aspernatur blanditiis ex hic labore omnis!
-                            </div>
-                        </li>
-                    </ul>
+                    {this.props.children}
                     <div className="confirmBlockForUser__content_btn">
-                        <span className="confirmBlockForUser__content_btn_ok">Ок</span>
-                        <span className="confirmBlockForUser__content_btn_cancel">Отмена</span>
+                        <span onClick={this.okHandler.bind(this)} className="confirmBlockForUser__content_btn_ok">Ок</span>
+                        <span onClick={this.cancelHandler.bind(this)} className="confirmBlockForUser__content_btn_cancel">Отмена</span>
                     </div>
                 </div>
             </div>
--- src/components/shop/pages/order/WaysDevPay.js
+++ src/components/shop/pages/order/WaysDevPay.js
@@ -161,8 +161,34 @@ class WaysDevPay extends React.Component {
                     }
 
                 </div>
-                <Confirm >
-
+                <Confirm
+                    okHandler={()=> console.log('Hello from parrent OK')}
+                    cancelHandler={()=> console.log('Hello from parrent cancel')}
+                >
+                    <ul>
+                        <li>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad amet beatae,
+                                debitis doloremque dolores ducimus facilis id laboriosam laudantium maiores mollitia,
+                                nemo
+                                nostrum numquam, quos suscipit vel velit voluptatem.
+                            </div>
+                            <div>Debitis dolorem odit optio suscipit. Ad animi asperiores assumenda beatae corporis
+                                culpa
+                                cumque debitis eos explicabo facere inventore laboriosam, maxime nihil quae quasi
+                                ratione
+                                repellat sint temporibus totam vitae voluptas!
+                            </div>
+                            <div>Assumenda cupiditate dolores earum eius facere, fugit maiores quasi, qui recusandae
+                                reiciendis rem repellendus repudiandae suscipit. Alias amet animi consectetur
+                                dignissimos
+                                excepturi incidunt ipsam, ipsum modi nulla porro saepe sapiente.
+                            </div>
+                            <div>Ab asperiores aut cupiditate, ducimus, eos facilis nemo non obcaecati quasi quidem quis
+                                tenetur vel, vero. Consequuntur, dolorum incidunt ipsum numquam possimus provident quas.
+                                Aspernatur blanditiis ex hic labore omnis!
+                            </div>
+                        </li>
+                    </ul>
                 </Confirm>
             </div>
         )
