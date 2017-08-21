9d54e5ec5432e7add758753b74194ec8078f2f23
DenysSidorov <000scorpions000@gmail.com>
2017-08-21 23:05:17 +0300

    add new code style for react component

--- src/components/shop/pages/Blog.js
+++ src/components/shop/pages/Blog.js
@@ -1,14 +1,14 @@
 import React from "react";
-import st from './blog.scss';
 class Blog extends React.Component {
-        componentDidMount(prevProps) {
-                window.scrollTo(0, 0)
-        }
-    render() {
+    componentDidMount = (prevProps) => {
+        window.scrollTo(0, 0)
+    }
+
+    render = ()=>{
         return (
-         <div className="blogPage-container">
-                        <p>Постов нет !</p>
-         </div>
+            <div className="blogPage-container">
+                <p>Постов нет !</p>
+            </div>
         )
 
     }
