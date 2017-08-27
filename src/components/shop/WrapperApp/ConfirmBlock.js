67063c5acb9f12dcce7323cb2445fc563e4e36c5
DenysSidorov <000scorpions000@gmail.com>
2017-08-27 11:40:33 +0300

    fix in confirm show

--- src/components/shop/WrapperApp/ConfirmBlock.js
+++ src/components/shop/WrapperApp/ConfirmBlock.js
@@ -26,7 +26,7 @@ class ConfirmBlock extends React.Component {
 
                     {this.props.children}
                     <div className="confirmBlockForUser__content_btn">
-                        <span onClick={this.okHandler.bind(this)} className="confirmBlockForUser__content_btn_ok">Ок</span>
+                            <span onClick={this.okHandler.bind(this)} className="confirmBlockForUser__content_btn_ok">Ок</span>
                         <span onClick={this.cancelHandler.bind(this)} className="confirmBlockForUser__content_btn_cancel">Отмена</span>
                     </div>
                 </div>
