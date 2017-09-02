7ff66213e6bb1ced18fb47ee5c7bc5162da5fb1c
DenysSidorov <000scorpions000@gmail.com>
2017-09-02 14:18:23 +0300

    add auth reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,3 +2,19 @@ export const AUTH_USER = 'authReducer/AUTH_USER';
 export const UNAUTH_USER ='authReducer/UNAUTH_USER';
 export const AUTH_ERROR = 'authReducer/AUTH_ERROR';
 export const FETCH_MESSAGE = 'authReducer/FETCH_MESSAGE';
+
+
+export default function(state = {}, action) {
+    switch(action.type) {
+        case AUTH_USER:
+            return { ...state, error: '', authenticated: true };
+        case UNAUTH_USER:
+            return { ...state, authenticated: false };
+        case AUTH_ERROR:
+            return { ...state, error: action.payload };
+        case FETCH_MESSAGE:
+            return { ...state, message: action.payload };
+    }
+
+    return state;
+}
