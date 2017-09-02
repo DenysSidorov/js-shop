cef9f52a0195ff6e7028e5aa0d8482be5d1608bd
DenysSidorov <000scorpions000@gmail.com>
2017-09-02 14:07:53 +0300

    add action types

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,4 @@
-/**
- * Created by Denis on 02.09.2017.
- */
+export const AUTH_USER = 'authReducer/AUTH_USER';
+export const UNAUTH_USER ='authReducer/UNAUTH_USER';
+export const AUTH_ERROR = 'authReducer/AUTH_ERROR';
+export const FETCH_MESSAGE = 'authReducer/FETCH_MESSAGE';
