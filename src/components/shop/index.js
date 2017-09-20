15ab26e04be28f01ae524a49ed830602f4b5ae36
Denys Sidorov <000scorpions000@gmail.com>
2017-09-20 16:40:14 +0300

    admin panel init

--- src/components/shop/index.js
+++ src/components/shop/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import WrapperApp from "./WrapperApp";
 import axios from "axios";
-
+import {Route, Switch} from "react-router-dom";
+import RouteAuth from "../shop/helpers/RouteAuth";
+import Panel from '../shop/panel/index';
 class App extends React.Component {
     async componentDidMount() {
         // TODO getTime, isAuth, getCurrency, getName, getDate, getLocation, getSomeData
@@ -14,7 +16,11 @@ class App extends React.Component {
 
         return (
             <div style={{width: '100%', height: '100%'}}>
-                <WrapperApp {...this.props}/>
+                <Switch>
+                    <RouteAuth path="/panel" component={Panel}/>
+                    <WrapperApp {...this.props}/>
+                </Switch>
+
             </div>
         )
     }
--- src/components/shop/panel/index.js
+++ src/components/shop/panel/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import queryParams from '../helpers/lib/queryParams';
+import styles from './index.scss';
 class Panel extends React.Component {
     componentDidMount = (prevProps) => {
         window.scrollTo(0, 0)
+
         // var token = queryParams['t'];
         // if (token) {
         //     console.log(token, 'tokenischeeeeeee');
@@ -12,7 +14,9 @@ class Panel extends React.Component {
 
     render = ()=> {
         return (
-           <div style={{fontSize: '1.6rem', fontFamily: 'Roboto-Regular'}}>Админ Панель</div>
+           <div className="adminPanContainer fullWidth left">
+               <div className="container">Админ Панель</div>
+           </div>
         )
 
     }
