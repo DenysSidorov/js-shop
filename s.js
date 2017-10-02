e180064ae7267d539aae14f6f5a3443dc335fef0
Denys Sidorov <000scorpions000@gmail.com>
2017-10-02 18:02:43 +0300

    add simple test server

--- s.js
+++ s.js
@@ -1,1 +1,13 @@
-console.log('I'.toLowerCase(), 'env');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.get('/test', function (req, res) {
+    res.json({'json-api': true});
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
