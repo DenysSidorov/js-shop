29f9630e1635f8439c47665139cf29a7f859880e
DenysSidorov <000scorpions000@gmail.com>
2017-10-03 00:04:47 +0300

    add port for heroku4

--- s.js
+++ s.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World222222!');
-});
-app.get('/test', function (req, res) {
-    res.json({'json-api': true});
-});
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+// var express = require('express');
+// var app = express();
+//
+// app.get('/', function (req, res) {
+//     res.send('Hello World222222!');
+// });
+// app.get('/test', function (req, res) {
+//     res.json({'json-api': true});
+// });
+//
+// app.listen(process.env.PORT || 3000, function () {
+//     console.log('Example app listening on port 3000!');
+// });
+console.log('jopa');
