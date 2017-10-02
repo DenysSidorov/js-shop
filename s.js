8989169605527e15b8049b7e237244c1daf1b683
DenysSidorov <000scorpions000@gmail.com>
2017-10-03 00:02:18 +0300

    add port for heroku3

--- s.js
+++ s.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World222222!');
 });
 app.get('/test', function (req, res) {
     res.json({'json-api': true});
