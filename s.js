be3d3d3d11b9a5acf0c4251eb081fe2d62cd7dc8
DenysSidorov <000scorpions000@gmail.com>
2017-10-02 23:51:28 +0300

    add port for heroku

--- s.js
+++ s.js
@@ -8,6 +8,6 @@ app.get('/test', function (req, res) {
     res.json({'json-api': true});
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
