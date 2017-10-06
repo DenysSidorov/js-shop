276229e87233c8abe4ea993537355b32145d1b5e
DenysSidorov <000scorpions000@gmail.com>
2017-10-06 22:27:32 +0300

    prepare node for production

--- webpack.config.js
+++ webpack.config.js
@@ -275,3 +275,11 @@ module.exports = config;
 // "eslintConfig": {
 //     "extends": "airbnb"
 // }
+
+//
+//
+// "babel-cli": "~6.24.1",
+//     "babel-preset-es2015": "~6.24.1",
+//     "babel-preset-stage-0": "~6.24.1",
+//     "babel-preset-stage-1": "~6.24.1",
+//     "babel-preset-stage-2": "~6.24.1",
