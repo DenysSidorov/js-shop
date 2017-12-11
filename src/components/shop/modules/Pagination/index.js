90dc10ed005b4892668b9cf5cf1debf84dee4d20
DenysSidorov <000scorpions000@gmail.com>
2017-12-12 00:05:27 +0200

    plugin good library for pagination with react

--- src/components/shop/modules/Pagination/index.js
+++ src/components/shop/modules/Pagination/index.js
@@ -1,20 +1,39 @@
 import React, { PropTypes } from 'react';
 import styles from './index.scss'
 import {Link} from "react-router-dom";
-
+import ReactPaginate from 'react-paginate';
 
 
 class Pagination extends React.Component {
     constructor(props) {
         super(props);
         this.state = { };
+    this.onPageChange = this.onPageChange.bind(this)
+    this.hrefBuilder = this.hrefBuilder.bind(this)
     }
 
+    onPageChange(args){
+        console.log(args, 'args');
+    }
+    hrefBuilder(args){
+        // console.log(args, 'args - hrefBuilder');
+    }
 
 
     render() {
         return (
-            <div></div>
+            <ReactPaginate
+                initialPage={2}
+                pageCount={30}
+                pageRangeDisplayed={8}
+                marginPagesDisplayed={1}
+                hrefBuilder={this.hrefBuilder}
+                onPageChange={this.onPageChange}
+                previousLabel={"previous"}
+                containerClassName="paginationContainerUL"
+                disabledClassName="disableLI"
+                activeClassName="activeLI"
+            />
         );
     }
 }
