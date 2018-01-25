57abb39d4248558cd6d42f29b97f2050d01ce1dc
Denys Sidorov <000scorpions000@gmail.com>
2018-01-25 23:13:43 +0200

    fix bug in head panel, add verify for that

--- src/components/shop/modules/MenuInfoSection/index.js
+++ src/components/shop/modules/MenuInfoSection/index.js
@@ -1,89 +1,117 @@
-import React, {PureComponent} from "react";;
+import React, {PureComponent} from "react";
+
+;
 import styles from './index.less';
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import {Link} from 'react-router-dom';
 import {connect} from "react-redux";
 import {signoutUser, isAdminFunc, isValidToken} from '../../../../reducers/authReducer/actions'
 import {bindActionCreators} from "redux";
+
 class MenuInfoSection extends React.PureComponent {
 
-    state = {time:0}
+    state = {time: 0}
 
-    verifyAdminLink(){
-          this.props.isAdminFunc();
+    verifyAdminLink() {
+        this.props.isAdminFunc().then(() => {
+            let authenticated = this.props.authenticated;
+            let isAdmin = this.props.isAdmin;
+            console.log(authenticated, 'authenticated'.toUpperCase());
+            console.log(isAdmin, 'isAdmin'.toUpperCase());
+        });
     }
-    componentDidMount(){
-       this.verifyAdminLink();
 
-        this.timer = setInterval(()=>{
-            this.setState({time: this.state.time + 1000})
-        },1000)
+    componentDidMount() {
+        this.verifyAdminLink();
+
+        this.timer = setInterval(() => {
+            this.setState({time: this.state.time + 500})
+        }, 500)
     }
-    componentDidUpdate(){
-      this.verifyAdminLink();
-        if(this.state.time >= 10000){
-            clearInterval( this.timer)
+
+    componentDidUpdate() {
+        // this.verifyAdminLink();
+        if (this.state.time >= 5000) {
+            clearInterval(this.timer)
         }
     }
-    unAuth(){
+
+    componentWillUnmount(){
+        clearInterval(this.timer)
+    }
+
+    unAuth() {
         this.props.signoutUser();
     }
+
     render() {
         let authenticated = this.props.authenticated;
         let isAdmin = this.props.isAdmin;
-        console.log(authenticated, 'authenticated'.toUpperCase());
-        console.log(isAdmin, 'isAdmin'.toUpperCase());
-        if(this.state.time < 1500) return null;
+        // if(this.state.time < 1500) return null;
         return (
-            <div className="menuInfoSection left fullWidth ">
-                <div className="container">
-                    <div className="submenuBlock">
-                        <div className="contactInfo">
-                            <ul className="contactInfo__list-contacts-info">
-                                <ReactCSSTransitionGroup
-                                    transitionName="menuInfoSection"
-                                    transitionAppear={true}
-                                    transitionAppearTimeout={300}
-                                    transitionEnterTimeout={300}
-                                    transitionLeave={false}>
-                                <li>
-                                    <a title="Позвонить на Киевстар" href="tel:0988351315"><i
-                                        className="fa fa-phone"></i>&nbsp;(098)83-51-315</a>
-                                </li>
-                                <li>
-                                    <a title="Позвонить на Life)" href="tel:0938875395"><i className="fa fa-phone"></i>&nbsp;(093)88-75-395</a>
-                                </li>
-                                <li>
-                                    <a title="График работы, условия оплаты и доставки" href="#">
-                                        <meta content="Mo-Fr 10:00-19:00"/>
-                                            <i className="fa fa-clock-o"></i>&nbsp;10:00-19:00&nbsp;пн-пт</a>
-                                </li>
-                                </ReactCSSTransitionGroup>
-                            </ul>
-                        </div>
-                        {/*<div className="logoInfo">*/}
+          <div className="menuInfoSection left fullWidth ">
+              <div className="container">
+                  <div className="submenuBlock">
+                      {this.state.time < 1000
+                        ? null
+                        : <div className="submenuBlock_container">
+                            <div className="contactInfo">
+                                <ul className="contactInfo__list-contacts-info">
+                                    <ReactCSSTransitionGroup
+                                      transitionName="menuInfoSection"
+                                      transitionAppear={true}
+                                      transitionAppearTimeout={300}
+                                      transitionEnterTimeout={300}
+                                      transitionLeave={false}>
+                                        <li>
+                                            <a title="Позвонить на Киевстар"
+                                               href="tel:0988351315"><i
+                                              className="fa fa-phone"></i>&nbsp;(098)83-51-315</a>
+                                        </li>
+                                        <li>
+                                            <a title="Позвонить на Life)" href="tel:0938875395"><i
+                                              className="fa fa-phone"></i>&nbsp;(093)88-75-395</a>
+                                        </li>
+                                        <li>
+                                            <a title="График работы, условия оплаты и доставки"
+                                               href="#">
+                                                <meta content="Mo-Fr 10:00-19:00"/>
+                                                <i className="fa fa-clock-o"></i>&nbsp;
+                                                10:00-19:00&nbsp;пн-пт</a>
+                                        </li>
+                                    </ReactCSSTransitionGroup>
+                                </ul>
+                            </div>
+                            {/*<div className="logoInfo">*/}
                             {/*<img className="logoInfo__logo-img" src="/img-static/good.jpg" alt=""/>*/}
-                        {/*</div>*/}
-                        <div className="userInfo">
-                            <ul className="userInfo__listInfo">
-                                <ReactCSSTransitionGroup
-                                    transitionName="menuInfoSectionRigth"
-                                    transitionAppear={true}
-                                    transitionAppearTimeout={300}
-                                    transitionEnterTimeout={300}
-                                    transitionLeave={false}>
-                                {/*{isAdmin && <li><Link to='/panel'>Ввойти в кабинет</Link></li>}*/}
-                                {authenticated && isAdmin && <li><Link to='/panel'>Админка</Link></li>}
-                                {authenticated && <li><Link to='/profile'>Профиль юзера</Link></li>}
-                                {authenticated && <li><a onClick={this.unAuth.bind(this)}>Выйти</a></li>}
-                                {!authenticated && <li><Link to="/logup">Регистрация</Link></li>}
-                                {!authenticated && <li><Link to="/login">Войти</Link></li>}
-                                </ReactCSSTransitionGroup>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            {/*</div>*/}
+                            <div className="userInfo">
+                                <ul className="userInfo__listInfo">
+                                    <ReactCSSTransitionGroup
+                                      transitionName="menuInfoSectionRigth"
+                                      transitionAppear={true}
+                                      transitionAppearTimeout={300}
+                                      transitionEnterTimeout={300}
+                                      transitionLeave={false}>
+                                        {/*{isAdmin && <li><Link to='/panel'>Ввойти в кабинет</Link></li>}*/}
+                                        {authenticated && isAdmin &&
+                                        <li><Link to='/panel'>Админка</Link></li>}
+                                        {authenticated &&
+                                        <li><Link to='/profile'>Профиль юзера</Link></li>}
+                                        {authenticated &&
+                                        <li><a onClick={this.unAuth.bind(this)}>Выйти</a></li>}
+                                        {!authenticated &&
+                                        <li><Link to="/logup">Регистрация</Link></li>}
+                                        {!authenticated && <li><Link to="/login">Войти</Link></li>}
+                                    </ReactCSSTransitionGroup>
+                                </ul>
+                            </div>
+                        </div>}
+                  </div>
+
+
+              </div>
+          </div>
         )
     }
 }
@@ -98,9 +126,9 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return bindActionCreators({
-        signoutUser: ()=> signoutUser(),
-      isAdminFunc: ()=> isAdminFunc(),
-        isValidToken: ()=> isValidToken()
+        signoutUser: () => signoutUser(),
+        isAdminFunc: () => isAdminFunc(),
+        isValidToken: () => isValidToken()
 
     }, dispatch)
 }
