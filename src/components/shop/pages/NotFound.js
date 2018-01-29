685136a7c8acb6012bca7b9caec0c5dba61057c7
Denys Sidorov <000scorpions000@gmail.com>
2018-01-29 11:20:06 +0200

    change style-type of 404 page

--- src/components/shop/pages/NotFound.js
+++ src/components/shop/pages/NotFound.js
@@ -1,3 +1,3 @@
 import React from 'react';
-const NotFound = (props)=> {window.scrollTo(0, 0);return(<div>404 Sorry! Not Found Page with route: {props.location.pathname}</div>)}
+const NotFound = (props)=> {window.scrollTo(0, 0);return(<h1>404 Sorry! Not Found Page with route: {props.location.pathname}</h1>)}
 export default NotFound;
