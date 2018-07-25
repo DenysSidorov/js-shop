30521301401ffa2b8c13d157439b9e0a4d4967bb
Denys Sidorov <000scorpions000@gmail.com>
2018-07-26 00:03:29 +0300

    change card of good on main page

--- src/components/shop/modules/CardMainPage/index.js
+++ src/components/shop/modules/CardMainPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React  from "react";
 import style from './mainBodyCard.less';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -11,27 +11,32 @@ class CardMainPage extends React.Component {
 
         return (
             <div className="bodyCardItems__oneCardItem">
-                <div className="oneCardItem__headCard">
-                    <div className="oneCardItem__headCard__priceCard">
-                        <span>{card.price}</span><span></span><span>грн.</span>
-                    </div>
-                    <Link to={`/card/${card._id}`} className="oneCardItem__headCard__nameBrand">{card.name} {card.model}</Link>
-                    <div className="oneCardItem__headCard__wrap-things">
-                        {/*<div className="oneCardItem__headCard__otherThings">*/}
-                            {/*<i className="fa fa-heart"></i>*/}
-                            {/*<span className="oneCardItem__headCard__otherThings_like"> {card.likes}</span>*/}
-                        {/*</div>*/}
-                        <div className="oneCardItem__headCard__view">
-                            <i className="fa fa-eye" aria-hidden="true"></i>
-                            <span className="oneCardItem__headCard__view_items"> {card.views}</span>
-                        </div>
-                    </div>
-                </div>
+               <Link to={`/card/${card._id}`}>
+                   <div className="oneCardItem__headCard">
+                       <div className="oneCardItem__headCard__priceCard">
+                           <span>{card.price}</span><span></span><span>грн.</span>
+                       </div>
+                       <div  className="oneCardItem__headCard__nameBrand">
+                         {card.name} {card.model}
+                       </div>
+                       <div className="oneCardItem__headCard__wrap-things">
+                         {/*<div className="oneCardItem__headCard__otherThings">*/}
+                         {/*<i className="fa fa-heart"></i>*/}
+                         {/*<span className="oneCardItem__headCard__otherThings_like"> {card.likes}</span>*/}
+                         {/*</div>*/}
+                           <div className="oneCardItem__headCard__view">
+                               <i className="fa fa-eye" aria-hidden="true"></i>
+                               <span className="oneCardItem__headCard__view_items"> {card.views}</span>
+                           </div>
+                       </div>
+                   </div>
 
-                <div className="oneCardItem__imageBody">
-                    <img src={`/img-static/${card.photo[0]}`} alt=""/>
-                    <img src={`/img-static/${card.photo[1]}`} alt="" className="img-top"/>
-                </div>
+                   <div className="oneCardItem__imageBody">
+                       <img src={`/img-static/${card.photo[0]}`} alt=""/>
+                       <img src={`/img-static/${card.photo[1]}`} alt="" className="img-top"/>
+                   </div>
+               </Link>
+
 
                 <div className="oneCardItem__bottomCard">
                     <Link to={`/card/${card._id}`} className="oneCardItem__bottomCard__shortText">
