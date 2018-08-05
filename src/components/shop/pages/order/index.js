9b4633a5ddbf86190f83a468ae3a4a914adb7f81
Denys Sidorov <000scorpions000@gmail.com>
2018-08-06 00:19:19 +0300

    add buy-in-one-click in order page for all products

--- src/components/shop/pages/order/index.js
+++ src/components/shop/pages/order/index.js
@@ -3,59 +3,76 @@ import MenuInfoSection from "../../modules/MenuInfoSection";
 import WaysDevPay from "./WaysDevPay";
 import GoodsTable from "./GoodsTable";
 import {connect} from "react-redux";
-// CardMainPage
+import OneClickModal from '../../modules/modals/one-click-modal';
 import './index.less';
+
 class OrderPage extends React.Component {
-    state = {};
 
-    constructor(props) {
-        super(props);
-        this.initCadd = this.initCadd.bind(this);
+  state = {
+    isShowOneClickModal: false
+  }
 
+  handleShowOneClick = (state) => {
+    if (state === undefined) {
+      this.setState({
+        isShowOneClickModal: !this.state.isShowOneClickModal
+      })
+    } else {
+      this.setState({
+        isShowOneClickModal: state
+      })
     }
+  }
 
-    async initCadd() {
-        window.scrollTo(0, 0)
-    }
+  constructor(props) {
+    super(props);
+    this.initCadd = this.initCadd.bind(this);
 
-    componentDidMount() {
-        this.initCadd();
-    }
+  }
 
-    render() {
-        var styles = {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            flexDirection: 'column'
-        };
-        let goods = this.props.cart;
-        return (
-            <div style={styles}>
-                <MenuInfoSection/>
-                <div className="orderPage_fast">
-                    <span>Купить без заполнения формы!</span>
-                </div>
-                {!goods.length
-                    ? <div style={{padding: '30px'}}>
-                    <span style={{fontSize: '2rem'}}>Корзина пуста!</span>
-                </div>
-                    : <div className="formOrderMain">
-                    <GoodsTable/>
-                    <WaysDevPay {...this.props}/>
-                </div> }
+  async initCadd() {
+    window.scrollTo(0, 0)
+  }
 
-            </div>
+  componentDidMount() {
+    this.initCadd();
+  }
 
+  render() {
+    var styles = {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    };
+    let goods = this.props.cart;
+    return (
+      <div style={styles}>
+        <MenuInfoSection/>
+        <div className="orderPage_fast">
+          <span onClick={this.handleShowOneClick}>Купить без заполнения формы!</span>
+        </div>
+        {!goods.length
+          ? <div style={{padding: '30px'}}>
+            <span style={{fontSize: '2rem'}}>Корзина пуста!</span>
+          </div>
+          : <div className="formOrderMain">
+            <GoodsTable/>
+            <WaysDevPay {...this.props}/>
+          </div>}
+        {this.state.isShowOneClickModal && <OneClickModal
+          close={this.handleShowOneClick}
+          goods={goods}
+        />}
+      </div>
+    )
 
-        )
-
-    }
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        cart: state.cart.items
-    }
+  return {
+    cart: state.cart.items
+  }
 }
 export default connect(mapStateToProps)(OrderPage);
