2dcfa08c8f3d5eece1587c2b935d1cb36e1ce5bd
Denys Sidorov <000scorpions000@gmail.com>
2019-03-16 23:58:11 +0200

    add test route

--- app-server/shop/routes/user.js
+++ app-server/shop/routes/user.js
@@ -3,5 +3,6 @@ import * as UserController from '../controllers/user';
 import checkToken from './../../middlewares/checkToken'; // Проверка наличия токена
 const router = express.Router();
 router.get('/current', checkToken, UserController.getCurrentUser)
+router.get('/test', checkToken, UserController.getCurrentUser)
 
 export default router;
