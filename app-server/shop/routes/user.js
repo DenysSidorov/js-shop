0d74ffde3e6b0be04bfc00eb4077e0253c55160e
Denys Sidorov <000scorpions000@gmail.com>
2019-03-18 22:52:05 +0200

    fast fix

--- app-server/shop/routes/user.js
+++ app-server/shop/routes/user.js
@@ -2,7 +2,6 @@ import express from 'express';
 import * as UserController from '../controllers/user';
 import checkToken from './../../middlewares/checkToken'; // Проверка наличия токена
 const router = express.Router();
-router.get('/current', checkToken, UserController.getCurrentUser)
-router.get('/test', checkToken, UserController.getCurrentUser)
+router.get('/current', checkToken, UserController.getCurrentUser);
 
 export default router;
