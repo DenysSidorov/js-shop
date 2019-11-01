fef6504dfb914ba1be831fd1d3c052da8041fd9d
Denys Sidorov <000scorpions000@gmail.com>
2019-11-01 20:30:53 +0200

    http to https, v8

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.use('/api/users', userRoute);
 //   res.render(path.join(__dirname + '/www/main.ejs'));
 // });
 
-app.get('*', httpsMiddleWare, (req, res) => {
+app.get('*', (req, res) => {
 
   res.render(path.join(__dirname + '/www/index.ejs'), {assets});
   // res.sendFile(path.join(__dirname+'/www/index.ejs'));
