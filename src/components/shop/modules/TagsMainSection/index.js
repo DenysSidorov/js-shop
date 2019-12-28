c3328b0fb1353c685343f37524d249f66e9643ae
Denys Sidorov <000scorpions000@gmail.com>
2019-12-29 00:30:33 +0200

    fix categories's counts

--- src/components/shop/modules/TagsMainSection/index.js
+++ src/components/shop/modules/TagsMainSection/index.js
@@ -1,38 +1,41 @@
 import React from "react";
 import styles from './index.less'
 import {Link} from "react-router-dom";
+
 class TagsMainSection extends React.Component {
-    render() {
-        return (
-            <div className="itemsSection left fullWidth ">
-                <div className="container">
-                    <div className="themeItemsblock">
-                        <Link to={{
-                            pathname: '/shop',
-                            search: '?sort=main',
-                            hash: '',
-                            state: {fromDashboard: true}
-                        }} className="themeItemsblock__oneItem">
-                            <span className="themeItemsblock__oneItem_name">Все</span>
-                        </Link>
+  render() {
+    return (
+      <div className="itemsSection left fullWidth ">
+        <div className="container">
+          <div className="themeItemsblock">
+            <Link to={{
+              pathname: '/shop',
+              search: '?sort=main',
+              hash: '',
+              state: {fromDashboard: true}
+            }} className="themeItemsblock__oneItem">
+              <span className="themeItemsblock__oneItem_name">Все</span>
+            </Link>
 
 
-                        {this.props.uniqCategory.map((el, index) =>
-                            <Link key={index} to={{
-                                pathname: '/shop',
-                                search: `?sort=${el.name}`,
-                                hash: '',
-                                state: {fromDashboard: true}
-                            }} className="themeItemsblock__oneItem">
-                                <span className="themeItemsblock__oneItem_name">{el.name}{' '}</span>
-                                <span className="themeItemsblock__oneItem_count">{el.count}</span>
-                            </Link>
-                        )}
+            {this.props.uniqCategory.map((el, index) =>
+              <Link key={index} to={{
+                pathname: '/shop',
+                search: `?sort=${el.name}`,
+                hash: '',
+                state: {fromDashboard: true}
+              }} className="themeItemsblock__oneItem">
+                <span
+                  className="themeItemsblock__oneItem_name">{el.name.slice(0, 1).toUpperCase()}{el.name.slice(1, el.name.length)}{' '}</span>
+                <span className="themeItemsblock__oneItem_count">{el.count}</span>
+              </Link>
+            )}
 
-                    </div>
-                </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
 export default TagsMainSection;
