d469ca6f89a1da3a0625de8e39209a82232dfb23
Denys Sidorov <000scorpions000@gmail.com>
2020-03-07 16:02:58 +0200

    front's updating, step 1

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,32 @@
 module.exports = {
     plugins: {
-        'postcss-smart-import': {},
-        //'precss':{/* ...options */},
-        // 'autoprefixer': { browsers: ['> 1%', 'ie >= 8'],},
-        'autoprefixer': {},
-        // 'autoprefixer': { browsers: ['> 1%']},
-        'postcss-mixins':{}, // это не тестировал!
+        'postcss-import': {},
+        // add prefixes for old and different browsers
+        autoprefixer: {}
+        //  new syntax in developming http://cssnext.io/
+        // cssnext: {},
+        // cssnext: postcssPresetEnv(),
+        //  check syntax, optimization,  etc.   http://cssnano.co/
 
-        //  испольвание нового синтаксиса в css        http://cssnext.io/
-          'cssnext': {/* ...options */},
-        //  проверка синтаксиса, оптимизация  и т.д.   http://cssnano.co/
-        // 'cssnano': {/* ...options */},
+        // cssnano: {}
     }
-
 };
+
+
+
+// module.exports = {
+//     plugins: {
+//         'postcss-smart-import': {},
+//         //'precss':{/* ...options */},
+//         // 'autoprefixer': { browsers: ['> 1%', 'ie >= 8'],},
+//         'autoprefixer': {},
+//         // 'autoprefixer': { browsers: ['> 1%']},
+//         'postcss-mixins':{}, // это не тестировал!
+//
+//         //  испольвание нового синтаксиса в css        http://cssnext.io/
+//           'cssnext': {/* ...options */},
+//         //  проверка синтаксиса, оптимизация  и т.д.   http://cssnano.co/
+//         // 'cssnano': {/* ...options */},
+//     }
+//
+// };
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  semi: true
+};
