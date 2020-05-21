7b2d06d6837ef1e8bb8cc158669d7b2de0f30d13
Denys Sidorov <000scorpions000@gmail.com>
2020-05-22 00:52:27 +0300

    2, add webpack, babel, init app, configs

---
+++ front/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>You just setup react with babel and eslint</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ front/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'app_bin.js',
+  },
+  devServer: {
+    inline: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
