c4d628e173c3d4b78f57d7b623a7c793fe78aac9
Denys Sidorov <000scorpions000@gmail.com>
2020-05-23 00:38:08 +0300

    2  Babel, Webpack Hot Replacement (REACT), in progress v2

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>You just setup react with babel and sadsdasdasdasdadssad</h1>
+      <h1>You just setup react with babel and webpack</h1>
     </div>
   );
 };
