d7c1f82bb8bc15d37a32e4ae6fcc4871e3ec7075
Denys Sidorov <000scorpions000@gmail.com>
2020-05-28 22:42:55 +0300

    6, add Prettier, and bridge to ESLint

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,31 +1,36 @@
 // import { hot } from 'react-hot-loader/root';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Test from './Test';
 import './index.css';
-
 
 class App extends Component {
   state = {
     number: 1,
     b: 2
-  }
+  };
 
   clickHandler = () => {
     this.setState((prevState) => ({
       number: prevState.number + prevState.b
     }));
-  }
+  };
 
   render() {
-    const { number } = this.state;
+    const {number} = this.state;
     return (
       <div>
         <p onClick={this.clickHandler}>++++</p>
-        <h1>
-          We just setup react with babel1. Test is
-          {number}
-        </h1>
-        <Test />
+        <h1>We just setup react with babel1. Test is -{number}</h1>
+        <Test
+          aasdadadadsdas={21}
+          dfsafsafsadfsfb={6}
+          sadffsafsaasc={72341412}
+          aasdadadadsda2s={21}
+          dfsafsaf2sadfsfb={6}
+          sadffsaf2saasc={72341412}
+        >
+          sdf
+        </Test>
       </div>
     );
   }
--- front/src/components/Test.js
+++ front/src/components/Test.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Test = () => {
   const [count] = useState(19);
