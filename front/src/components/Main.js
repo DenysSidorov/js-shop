73c887942fc69b561193aaa2fa6cbe3c294aeec3
Denys Sidorov <000scorpions000@gmail.com>
2020-06-02 23:31:38 +0300

    remove unusable properties

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -24,16 +24,7 @@ class Main extends Component {
       <div>
         <p onClick={this.clickHandler}>++++</p>
         <h1>We just setup react with babel1. Test is -{number}</h1>
-        <Test
-          aasdadadadsdas={21}
-          dfsafsafsadfsfb={6}
-          sadffsafsaasc={723414412}
-          aasdadadadsda2s={21}
-          dfsafsaf2sadfsfb={6}
-          sadffsaf2saasc={72341412}
-        >
-          sdf
-        </Test>
+        <Test aasdadadadsdas={21}>sdf</Test>
         <div className='dfdf'>23</div>
         <div className='aaa'>1234</div>
         <img src={b} />
