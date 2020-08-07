33356b3e4887ac753d0092fa74756845b18aa831
Denys Sidorov <000scorpions000@gmail.com>
2020-08-07 22:59:29 +0300

    add react, redux, router and start app

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,13 +1,44 @@
+import {hot} from 'react-hot-loader/root';
 import React from 'react';
 import ReactDom from 'react-dom';
-import Main from './components/Main';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {createStore} from 'redux';
+// import {store} from './redux/store/configureStore';
+// import Wrapper from './components/Wrapper';
 
-const a = require('react-hot-loader/root');
+const Wrapper = () => {
+  return <div>Test</div>;
+};
 
-const AppWrapper = a.hot(Main);
+const App = hot(Wrapper);
 
-ReactDom.render(<AppWrapper />, document.getElementById('main'));
+const container = document.getElementById('main');
+if (container) {
+  ReactDom.render(
+    <Provider store={createStore((state) => state)}>
+      <div>
+        <Router>
+          <App />
+        </Router>
+      </div>
+    </Provider>,
+    container
+  );
+}
 
-// if(module.hot){
-//   module.hot.accept()
-// }
+console.log(process.env.NODE_ENV, ' MODE IN REACT');
+
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import Main from './components/Main';
+//
+// const a = require('react-hot-loader/root');
+//
+// const AppWrapper = a.hot(Main);
+//
+// ReactDom.render(<AppWrapper />, document.getElementById('main'));
+//
+// // if(module.hot){
+// //   module.hot.accept()
+// // }
