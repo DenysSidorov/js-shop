48d63a656ec359d4b41e8bbf97c8a2bb7b84a697
Denys Sidorov <000scorpions000@gmail.com>
2020-08-10 00:16:36 +0300

    change LandingPage component, v2

---
+++ front/src/helpers/libs/utils.tsx
@@ -0,0 +1,30 @@
+export const checkTextLength = (text: string, maxLength: number): string => {
+  let localText = text;
+  if (localText.length > maxLength) {
+    localText = `${localText.substr(0, maxLength - 3)}...`;
+    return localText;
+  }
+  return text;
+};
+
+export const setTitle = (title: string): void => {
+  const shopName = 'DOSHKI.COM | Картины на дереве в Украине | Картины на досках Украина';
+  const pageTitle = `${title} - ${shopName}`;
+  window.document.title = pageTitle;
+};
+
+export const setMetaTag = (name = 'description', value: any) => {
+  const content =
+    'деревяные картины в украине, интернет-магазин картин, украинские картины, картины для интерьера, картины на дереве, картины на досках, doshki.com, картины украина, деревянные картины';
+
+  const tag = document.querySelector(`meta[name="${name}"]`);
+
+  if (tag) {
+    tag.content = value || content;
+  } else {
+    const m = document.createElement('meta');
+    m.name = name;
+    m.content = value || content;
+    document.head.appendChild(m);
+  }
+};
