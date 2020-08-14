393c5546b48560b2480466e0c7944aea2017e89f
Denys Sidorov <000scorpions000@gmail.com>
2020-08-14 14:21:25 +0300

    improve Buy-Btn component, without full functionality

--- front/src/components/pages/landing/buy-btn/index.tsx
+++ front/src/components/pages/landing/buy-btn/index.tsx
@@ -9,16 +9,17 @@ interface IBuyBtn {
 const BuyBtn: FC<IBuyBtn> = ({text}) => {
   const [isShowOneClickModal, setIsShowOneClickModal] = useState(false);
 
-  function handleShowOneClick(value: boolean | undefined | React.MouseEvent): void {
-    console.log('value ====', value);
-    if (value === undefined) {
-      setIsShowOneClickModal(!isShowOneClickModal);
-    } else if (typeof value === 'boolean') {
-      setIsShowOneClickModal(value);
-    }
-  }
-
-  const showHandler = useCallback(handleShowOneClick, [isShowOneClickModal]);
+  const showHandler = useCallback(
+    (value: boolean | undefined | React.MouseEvent): void => {
+      console.log('value ====', value);
+      if (value === undefined) {
+        setIsShowOneClickModal(!isShowOneClickModal);
+      } else if (typeof value === 'boolean') {
+        setIsShowOneClickModal(value);
+      }
+    },
+    [isShowOneClickModal]
+  );
   console.log('RENDER Buy-BTN');
   return (
     <div className='buyNowContBodyTextContent2' onClick={() => showHandler(true)}>
