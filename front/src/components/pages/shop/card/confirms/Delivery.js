0ca98c41c2d5c1fee8ddf615db317183a8a6024d
Denys Sidorov <000scorpions000@gmail.com>
2020-09-13 16:53:00 +0300

    add modal windows on Card's page

--- front/src/components/pages/shop/card/confirms/Delivery.js
+++ front/src/components/pages/shop/card/confirms/Delivery.js
@@ -1,25 +1,22 @@
-import React from "react";
-import st from './index.scss';
-class Delivery extends React.Component {
+import React from 'react';
+import './index.scss';
 
+const Delivery = () => {
+  return (
+    <div className='exchangeContainer'>
+      <div className='exchangeContainer__title'>Быстрая доставка</div>
+      <div>
+        <p className='exchangeContainer__desc'>
+          Мы предоставляем все самые удобные виды доставки, вам остается лишь выбрать лучший для вас.
+        </p>
 
+        <p className='exchangeContainer__titleSub'>Какие варианты доставки?</p>
 
-    render() {
-        return (
-            <div className="exchangeContainer">
-                <div className="exchangeContainer__title">Быстрая доставка</div>
-                <div>
-                    <p className="exchangeContainer__desc">Мы предоставляем все самые удобные виды доставки, вам остается лишь выбрать лучший для вас.</p>
-
-                    <p className="exchangeContainer__titleSub">Какие варианты доставки?</p>
-
-                    <p className="exchangeContainer__item">- Доставка по Украине "Новой Почтой"</p>
-                    <p className="exchangeContainer__item">- Доставка по Украине "Интайм"</p>
-                </div>
-            </div>
-        )
-
-    }
-}
+        <p className='exchangeContainer__item'>- Доставка по Украине "Новой Почтой"</p>
+        <p className='exchangeContainer__item'>- Доставка по Украине "Интайм"</p>
+      </div>
+    </div>
+  );
+};
 
 export default Delivery;
--- front/src/components/pages/shop/card/confirms/Payment.js
+++ front/src/components/pages/shop/card/confirms/Payment.js
@@ -1,25 +1,22 @@
-import React from "react";
-import st from './index.scss';
-class Payment extends React.Component {
+import React from 'react';
+import './index.scss';
 
+const Payment = () => {
+  return (
+    <div className='exchangeContainer'>
+      <div className='exchangeContainer__title'>Удобная оплата</div>
+      <div>
+        <p className='exchangeContainer__desc'>
+          Для удобства, мы подобрали для вас самые разнообразные способы оплаты, от онлайн платежей до оплаты по факту.
+        </p>
 
+        <p className='exchangeContainer__titleSub'>Вы можете оплатить:</p>
 
-    render() {
-        return (
-            <div className="exchangeContainer">
-                <div className="exchangeContainer__title">Удобная оплата</div>
-                <div>
-                    <p className="exchangeContainer__desc">Для удобства, мы подобрали для вас самые разнообразные способы оплаты, от онлайн платежей до оплаты по факту.</p>
-
-                    <p className="exchangeContainer__titleSub">Вы можете оплатить:</p>
-
-                    <p className="exchangeContainer__item">- Онлайн-платежом   (Visa/MasterCard, Приват24, LiqPay, Монобанк)</p>
-                    <p className="exchangeContainer__item">- Наличными в офисе Новой Почты или ИнТайм</p>
-                </div>
-            </div>
-        )
-
-    }
-}
+        <p className='exchangeContainer__item'>- Онлайн-платежом (Visa/MasterCard, Приват24, LiqPay, Монобанк)</p>
+        <p className='exchangeContainer__item'>- Наличными в офисе Новой Почты или ИнТайм</p>
+      </div>
+    </div>
+  );
+};
 
 export default Payment;
