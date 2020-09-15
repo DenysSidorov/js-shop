f07994644e3eb6387aae832cd8a2c4c010df563a
Denys Sidorov <000scorpions000@gmail.com>
2020-09-15 23:18:13 +0300

    add similar goods on card page

--- front/src/components/pages/shop/card/Card.tsx
+++ front/src/components/pages/shop/card/Card.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import {RouteComponentProps} from 'react-router-dom';
 import urlApi from '../../../../api/urlApi';
 import MainContainerForCard from './MainContainerForCard';
-// import SimilarGoodsSection from '../../modules/SimilarGoodsSection';
+import SimilarGoodsSection from '../../../modules/similar-goods-section/SimilarGoodsSection';
 // import ContainerForCardAdditional from './ContainerForCardAdditional';
 import {setMetaTag, setTitle} from '../../../../helpers/libs/utils';
 
@@ -16,13 +16,13 @@ interface IHome extends RouteComponentProps<any> {
 interface SCardComponent {
   card: Array<{name: string; tags: any}>;
   popularCards: any;
-  // similarCategory: Array<any>;
+  similarCategory: any;
 }
 
 class CardComponent extends React.Component<IHome, SCardComponent> {
   state = {
     card: [],
-    // similarCategory: [],
+    similarCategory: [],
     popularCards: []
   };
 
@@ -60,17 +60,17 @@ class CardComponent extends React.Component<IHome, SCardComponent> {
       'интернет-магазин картин, украинские картины, картины для интерьера, картины на дереве, картины на досках, doshki.com, картины украина, деревянные картины, doshki.kom'
     );
 
-    // let similarCategory: any;
+    let similarCategory: any;
     let popularCards: any;
     let card: any;
     // TODO getTime, isAuth, getCurrency, getName, getDate, getLocation, getSomeData
     try {
       card = await axios.get(`${urlApi}/api/goods/${id}`);
-      // if (card.data.length) {
-      //   similarCategory = await axios.post(`${urlApi}/api/goods/${id}/similar`, {
-      //     params: {category: card.data[0].category}
-      //   });
-      // }
+      if (card.data.length) {
+        similarCategory = await axios.post(`${urlApi}/api/goods/${id}/similar`, {
+          params: {category: card.data[0].category}
+        });
+      }
       popularCards = await axios.get(`${urlApi}/api/goods/popular`);
     } catch (e) {
       console.log(e);
@@ -78,7 +78,7 @@ class CardComponent extends React.Component<IHome, SCardComponent> {
       this.setState(
         {
           card: card && card.data ? card.data : [],
-          // similarCategory: similarCategory && similarCategory.data ? similarCategory.data : [],
+          similarCategory: similarCategory && similarCategory.data ? similarCategory.data : [],
           popularCards: popularCards.data && popularCards.data ? popularCards.data : []
         },
         () => {
@@ -107,7 +107,7 @@ class CardComponent extends React.Component<IHome, SCardComponent> {
   }
 
   render() {
-    const {card, popularCards} = this.state;
+    const {card, popularCards, similarCategory} = this.state;
     const a: any = popularCards;
     a.push();
     // console.log(card, 'CART', card && card.length && card[0].model ? card[0].model.toUpperCase() : '');
@@ -117,9 +117,9 @@ class CardComponent extends React.Component<IHome, SCardComponent> {
         {card.length ? (
           <div>
             <MainContainerForCard card={card} />
-            {/* {this.state.similarCategory && this.state.similarCategory.length */}
-            {/* ? <SimilarGoodsSection cards={this.state.similarCategory} title={'Похожие'}/> */}
-            {/*: null} */}
+            {similarCategory && similarCategory.length ? (
+              <SimilarGoodsSection cards={similarCategory} title='Похожие' />
+            ) : null}
             {/* {popularCards && popularCards.length && card.length ? ( */}
             {/*  <ContainerForCardAdditional card={card} popularCards={popularCards} /> */}
             {/* ) : null} */}
