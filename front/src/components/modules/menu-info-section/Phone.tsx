2cc5f27e5b7867cee65360381df94a329d4fce23
Denys Sidorov <000scorpions000@gmail.com>
2020-10-02 23:05:12 +0300

    add hooks and typescript to Phone

--- front/src/components/modules/menu-info-section/Phone.tsx
+++ front/src/components/modules/menu-info-section/Phone.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 
-const Phone = (props: {phoneMobile: string; phoneText: string}) => {
-  // const [phoneForUser, setPhoneForUser] = useState(() => {
-  //   return `${props.phoneText.slice(0, 5)} Показать номер`;
-  // });
-  //
-  // const showAll = () => {
-  //   setPhoneForUser(props.phoneText);
-  // };
+interface IPhone {
+  phoneMobile: string;
+  phoneText: string;
+}
 
+const Phone = (props: IPhone) => {
   const {phoneMobile, phoneText} = props;
   return (
-    <a
-      title='Позвонить на Лайф'
-      href={phoneMobile}
-      className='contactInfo__list-contacts-info_time'
-      // onClick={this.showAll}
-    >
+    <a title='Позвонить на Лайф' href={phoneMobile} className='contactInfo__list-contacts-info_time'>
       <i className='fa fa-phone' />
       &nbsp;{phoneText}
     </a>
