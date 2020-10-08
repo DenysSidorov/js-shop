894cb1117139ebef5bfa5b6b44d9cfcdd34bfbb5
Denys Sidorov <000scorpions000@gmail.com>
2020-10-08 20:45:28 +0300

    add typescript and react's hooks to OrderPage component

--- front/src/components/pages/order/index.tsx
+++ front/src/components/pages/order/index.tsx
@@ -1,86 +1,89 @@
-import React, {CSSProperties} from 'react';
-import {connect} from 'react-redux';
-// import MenuInfoSection from '../../modules/MenuInfoSection';
+import React, {CSSProperties, FC, useCallback, useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
 import WaysDevPay from './WaysDevPay';
 import GoodsTable from './GoodsTable';
 import OneClickModal from '../../parts/modals/one-click-modal/OneClickModal';
 import './index.less';
 import {setMetaTag, setTitle} from '../../../helpers/libs/utils';
+import {ICartReducerItem} from '../../../redux/reducers/cart-reducer/cartReducer';
+import {IReducersState} from '../../../redux/reducers';
 
-interface SOrderPage {
-  isShowOneClickModal: boolean;
-}
+interface IOrderPage {}
 
-interface IOrderPage {
-  cart: any;
-}
+const OrderPage: FC<IOrderPage> = () => {
+  const [isShowOneClickModal, setIsShowOneClickModal] = useState<boolean>(false);
+  const goods: Array<ICartReducerItem> = useSelector((state: IReducersState) => state.cartReducer.items);
 
-class OrderPage extends React.Component<IOrderPage, SOrderPage> {
-  state = {
-    isShowOneClickModal: false
-  };
+  const initCard = useCallback(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-  componentDidMount() {
-    this.initCadd();
+  useEffect(() => {
+    initCard();
     setTitle('Корзина');
     setMetaTag('description', 'Корзина ваших товаров');
     setMetaTag(
       'keywords',
       'интернет-магазин картин, украинские картины, картины для интерьера, картины на дереве, картины на досках, doshki.com, doshki.kom, картины украина, деревянные картины'
     );
-  }
+  }, [initCard]);
 
-  handleShowOneClick = (state: any) => {
+  // componentDidMount() {
+  //   this.initCadd();
+  //   setTitle('Корзина');
+  //   setMetaTag('description', 'Корзина ваших товаров');
+  //   setMetaTag(
+  //     'keywords',
+  //     'интернет-магазин картин, украинские картины, картины для интерьера, картины на дереве, картины на досках, doshki.com, doshki.kom, картины украина, деревянные картины'
+  //   );
+  // }
+
+  const handleShowOneClick = useCallback((state?: boolean | undefined) => {
     if (state === undefined) {
-      this.setState((prevState) => ({isShowOneClickModal: !prevState.isShowOneClickModal}));
+      setIsShowOneClickModal((prevState) => !prevState);
     } else {
-      this.setState({
-        isShowOneClickModal: state
-      });
+      setIsShowOneClickModal(state);
     }
+  }, []);
+
+  // handleShowOneClick = (state: any) => {
+  //   if (state === undefined) {
+  //     this.setState((prevState) => ({isShowOneClickModal: !prevState.isShowOneClickModal}));
+  //   } else {
+  //     this.setState({
+  //       isShowOneClickModal: state
+  //     });
+  //   }
+  // };
+
+  const styles: CSSProperties = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    flexDirection: 'column'
   };
 
-  initCadd = async () => {
-    window.scrollTo(0, 0);
-  };
-
-  render() {
-    const styles: CSSProperties = {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      flexDirection: 'column'
-    };
-    const {cart: goods} = this.props;
-    return (
-      <div style={styles}>
-        {/* <MenuInfoSection /> */}
-        {goods.length > 0 && (
-          <div className='orderPage_fast'>
-            <span onClick={this.handleShowOneClick}>Купить без заполнения формы!</span>
-          </div>
-        )}
-        {!goods.length ? (
-          <div style={{padding: '30px'}}>
-            <span style={{fontSize: '2rem'}}>Корзина пуста!</span>
-          </div>
-        ) : (
-          <div className='formOrderMain'>
-            <GoodsTable />
-            <WaysDevPay {...this.props} />
-          </div>
-        )}
-        {this.state.isShowOneClickModal ? (
-          <OneClickModal close={this.handleShowOneClick} goods={goods} willDeleteGoods />
-        ) : null}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: any) => {
-  return {
-    cart: state.cartReducer.items
-  };
+  return (
+    <div style={styles}>
+      {/* <MenuInfoSection /> */}
+      {goods.length > 0 && (
+        <div className='orderPage_fast'>
+          <span onClick={() => handleShowOneClick()}>Купить без заполнения формы!</span>
+        </div>
+      )}
+      {!goods.length ? (
+        <div style={{padding: '30px'}}>
+          <span style={{fontSize: '2rem'}}>Корзина пуста!</span>
+        </div>
+      ) : (
+        <div className='formOrderMain'>
+          <GoodsTable />
+          <WaysDevPay />
+        </div>
+      )}
+      {isShowOneClickModal ? <OneClickModal close={handleShowOneClick} goods={goods} willDeleteGoods /> : null}
+    </div>
+  );
 };
-export default connect(mapStateToProps)(OrderPage);
+
+export default OrderPage;
