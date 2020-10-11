1e7beb543facbe6e012f5b1205563e49c55ba8cf
Denys Sidorov <000scorpions000@gmail.com>
2020-10-11 17:15:00 +0300

    add react's hooks and typescript to Panel (in admin panel)

--- front/src/components/pages/panel/Panel.tsx
+++ front/src/components/pages/panel/Panel.tsx
@@ -1,60 +1,48 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Switch, Route, useRouteMatch} from 'react-router-dom';
 import LoadingBar from 'react-redux-loading-bar';
 import AdminInfo from './AdminForPage';
 import Header from './Header';
 import Orders from './Orders';
 import LeftMenu from './LeftMenu';
+// import {useRouteMatch} from "react-router";
 
-interface IPanel {
-  location: any;
-}
+type Token = string | null;
 
-interface SPanel {
-  token: string | null;
-}
+const Panel = () => {
+  const [token, setToken] = useState<Token>('');
 
-class Panel extends React.Component<IPanel, SPanel> {
-  state = {
-    token: ''
-  };
-
-  componentDidMount = async () => {
+  useEffect(() => {
     window.scrollTo(0, 0);
 
     try {
-      const token: string | null = localStorage.getItem('info');
-      this.setState({token});
+      const tokenFromStorage: Token = localStorage.getItem('info');
+      setToken(tokenFromStorage);
     } catch (error) {
       console.error(error);
     }
-  };
+  }, []);
 
-  render = () => {
-    if (!this.state.token) return <div>Отказано в доступе</div>;
-    return (
-      <div className='adminPanContainer fullWidth left'>
-        <Header />
-        <LoadingBar style={{backgroundColor: '#ff7867', height: '3px'}} />
-        <div className='adminPan__mainContent'>
-          <LeftMenu />
-          <div className='adminPan__mainContent_content left'>
-            <Switch>
-              <Route exact path='/panel' component={Orders} />
-              <Route path='/panel/orders' component={Orders} />
-              <Route
-                path='/panel/test'
-                render={({match}) => {
-                  return <h1>Hello user 2 {match.params.username}!</h1>;
-                }}
-              />
-              <Route path='/panel/admin' component={AdminInfo} />
-            </Switch>
-          </div>
+  const match = useRouteMatch('/panel/test');
+  if (!token) return <div>Отказано в доступе</div>;
+
+  return (
+    <div className='adminPanContainer fullWidth left'>
+      <Header />
+      <LoadingBar style={{backgroundColor: '#ff7867', height: '3px'}} />
+      <div className='adminPan__mainContent'>
+        <LeftMenu />
+        <div className='adminPan__mainContent_content left'>
+          <Switch>
+            <Route exact path='/panel' component={Orders} />
+            <Route path='/panel/orders' component={Orders} />
+            <Route path='/panel/admin' component={AdminInfo} />
+          </Switch>
+          {match ? <h1>Hello user 2 {match.params.toString()}!</h1> : null}
         </div>
       </div>
-    );
-  };
-}
+    </div>
+  );
+};
 
 export default Panel;
