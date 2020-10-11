7d8bc5312a789c89ad31574e9c479f85c047af32
Denys Sidorov <000scorpions000@gmail.com>
2020-10-11 15:59:52 +0300

    add react's hooks and typescript to WaysDevPay component

--- front/src/components/pages/order/WaysDevPay.tsx
+++ front/src/components/pages/order/WaysDevPay.tsx
@@ -1,24 +1,13 @@
 import React, {useCallback, useRef, useState} from 'react';
-import {connect, useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 import Dropdown from 'react-dropdown';
-import {bindActionCreators} from 'redux';
-// import axios from 'axios';
-// import urlApi from '../../../../api/urlApi';
-// import {push} from 'react-router-redux';
-// import {RouteComponentProps} from 'react-router-dom';
-import {RouteComponentProps} from 'react-router-dom';
+import axios from 'axios';
 import Confirm from '../../parts/modals/confirm-cart-modal/ConfirmBlock';
-import {
-  // pushToCart as pushToCartFu,
-  // deleteFromCart as deleteFromCartFu,
-  // incrementItem as incrementItemFu,
-  // decrementItem as decrementItemFu,
-  deleteAll as deleteAllFu,
-  ICartReducerItem
-} from '../../../redux/reducers/cart-reducer/cartReducer';
+import {deleteAll as deleteAllFu, ICartReducerItem} from '../../../redux/reducers/cart-reducer/cartReducer';
 import GoodsTable from './GoodsTable';
 import {IReducersState} from '../../../redux/reducers';
-import {IHistory} from "../../../interfaces";
+import urlApi from '../../../api/urlApi';
 
 type PaymentObject = {
   value: string;
@@ -38,43 +27,20 @@ interface SWaysDevPay {
   paymentVariants: Array<PaymentObject>;
 }
 
-/**
- deleteItem: (item) => deleteFromCartFu(item),
- addItem: (item) => pushToCartFu(item),
- incrementItem: (id) => incrementItemFu(id),
- decrementItem: (id) => decrementItemFu(id),
- changePage: pushTo,
- deleteAll: () => deleteAllFu()
-
- changePage
- deleteAll
- * */
-
-// interface IHistory extends RouteComponentProps<any> {
-//   location: any;
-//   history: any;
-// }
-
-interface IWaysDevPay extends IHistory {
-  cart: any;
-  // pushToCart: Function;
-  // deleteFromCart: Function;
-  // incrementItem: Function;
-  // decrementItem: Function;
-
-  deleteAll: Function;
-  // location: any;
-  // history: any;
-  [key: string]: any;
-}
-
-const WaysDevPay = ({history}) => {
+const WaysDevPay = () => {
+  const history = useHistory();
   const phoneV: any = useRef<HTMLInputElement>(null);
   const addressV: any = useRef<HTMLInputElement>(null);
   const nameV: any = useRef<HTMLInputElement>(null);
   const emailV: any = useRef<HTMLInputElement>(null);
 
   const cartItems: Array<ICartReducerItem> = useSelector((state: IReducersState) => state.cartReducer.items);
+
+  const dispatch = useDispatch();
+
+  const deleteAll = useCallback(() => {
+    dispatch(deleteAllFu());
+  }, [dispatch]);
 
   const initialState = {
     payment: {value: 'predo', label: 'Предоплата на карту'},
@@ -93,50 +59,6 @@ const WaysDevPay = ({history}) => {
   };
 
   const [state, setState] = useState<SWaysDevPay>(initialState);
-
-  const chName = useCallback(
-    (e: any) => {
-      if (e.target.value.length < 70) {
-        setState((prev) => ({...prev, name: e.target.value}));
-        validateData();
-        // this.setState({name: e.target.value}, this.validateData());
-      }
-    },
-    [validateData]
-  );
-
-  const chPhone = useCallback(
-    (e: any) => {
-      if (e.target.value.length < 70) {
-        setState((prev) => ({...prev, phone: e.target.value}));
-        validateData();
-        // this.setState({phone: e.target.value}, this.validateData());
-      }
-    },
-    [validateData]
-  );
-
-  const chAddress = useCallback(
-    (e: any) => {
-      if (e.target.value.length < 70) {
-        setState((prev) => ({...prev, address: e.target.value}));
-        validateData();
-        // this.setState({address: e.target.value}, this.validateData());
-      }
-    },
-    [validateData]
-  );
-
-  const chEmail = useCallback(
-    (e: any) => {
-      if (e.target.value.length < 70) {
-        setState((prev) => ({...prev, email: e.target.value}));
-        validateData();
-        // this.setState({email: e.target.value});
-      }
-    },
-    [validateData]
-  );
 
   const validateData = useCallback((): any => {
     // const {phone, address, name} = state;
@@ -161,22 +83,57 @@ const WaysDevPay = ({history}) => {
     if (address.length < 4) {
       localArr.push(addressErr);
     }
-    // this.setState({orderInputErrors: [...orderInputErrors, nameErr]})
 
     if (localArr.length > 0) {
       setState((prevState) => ({...prevState, isNormal: false, orderInputErrors: [...localArr]}));
-      // this.setState({
-      //   isNormal: false,
-      //   orderInputErrors: [...localArr]
-      // });
     } else {
       setState((prevState) => ({...prevState, isNormal: true, orderInputErrors: []}));
-      // this.setState({
-      //   isNormal: true,
-      //   orderInputErrors: []
-      // });
     }
   }, []);
+
+  const chName = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const name = e.target.value;
+      if (name.length < 70) {
+        setState((prev) => ({...prev, name}));
+        validateData();
+      }
+    },
+    [validateData]
+  );
+
+  const chPhone = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const phone = e.target.value;
+      if (phone.length < 70) {
+        setState((prev) => ({...prev, phone}));
+        validateData();
+      }
+    },
+    [validateData]
+  );
+
+  const chAddress = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const address = e.target.value;
+      if (address.length < 70) {
+        setState((prev) => ({...prev, address}));
+        validateData();
+      }
+    },
+    [validateData]
+  );
+
+  const chEmail = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const email = e.target.value;
+      if (email.length < 70) {
+        setState((prev) => ({...prev, email}));
+        validateData();
+      }
+    },
+    [validateData]
+  );
 
   const handleConfirmUnmount = useCallback(() => {
     setState((prevState) => ({...prevState, isShowConfirm: false}));
@@ -197,16 +154,10 @@ const WaysDevPay = ({history}) => {
   const dispatchData = useCallback(() => {
     if (cartItems.length) {
       setState((prevState) => ({...prevState, isShowConfirm: true}));
-      // this.setState({
-      //   isShowConfirm: true
-      // });
     }
-
-    // console.log(this.state);
   }, [cartItems.length]);
 
   const chDelivery = useCallback((kind: string) => {
-    // console.log(kind, 'k');
     setState((prevState) => ({...prevState, delivery: kind}));
   }, []);
 
@@ -246,10 +197,10 @@ const WaysDevPay = ({history}) => {
       });
 
       try {
-        let response: any = {data: {_id: 987654321}, _id: 12324453};
+        let response: any = await axios.post(`${urlApi}/api/orders`, order);
         if (response) {
+          deleteAll();
           response = response.data;
-          // push('/about-us');
           changePage(response._id);
         }
       } catch (e) {
@@ -259,12 +210,9 @@ const WaysDevPay = ({history}) => {
       }
     };
     getData();
-  }, [state, cartItems, changePage]);
+  }, [deleteAll, state, cartItems, changePage]);
 
-  // const goods = this.props.cart;
   const {paymentVariants, delivery, name, address, email, phone, payment} = state;
-  // let payment = [{value: 'predo', label: 'Предоплата на карту'},
-  //   {value: 'naloj', label: 'Наложенный платеж'}]
   return (
     <div className='userWaysContainer'>
       <div className='userWays'>
@@ -415,7 +363,6 @@ const WaysDevPay = ({history}) => {
           </ul>
           <div className='maskForGoodsTableInOrder'>
             <div className='maskForGoodsTableInOrder-mask' />
-            {/* <GoodsTable cart={goods} /> */}
             <GoodsTable />
           </div>
         </Confirm>
@@ -424,33 +371,4 @@ const WaysDevPay = ({history}) => {
   );
 };
 
-const mapStateToProps = (state: any) => {
-  return {
-    cart: state.cartReducer.items
-  };
-};
-
-const mapDispatchToProps = (dispatch: any) => {
-  return bindActionCreators(
-    {
-      // deleteItem: (item) => deleteFromCartFu(item),
-      // addItem: (item) => pushToCartFu(item),
-      // incrementItem: (id) => incrementItemFu(id),
-      // decrementItem: (id) => decrementItemFu(id),
-      deleteAll: () => deleteAllFu()
-    },
-    dispatch
-  );
-};
-
-/**
- cart: any;
- pushToCart: Function;
- deleteFromCart: Function; //
- incrementItem: Function;
- decrementItem: Function;
- deleteAll: Function;
- changePage: Function;
- */
-
-export default connect(mapStateToProps, mapDispatchToProps)(WaysDevPay);
+export default WaysDevPay;
