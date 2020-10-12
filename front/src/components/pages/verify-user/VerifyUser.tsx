470902032d5e079e7ebe878ded432ba231398b52
Denys Sidorov <000scorpions000@gmail.com>
2020-10-12 23:00:20 +0300

    add react's hooks and typescript to VerifyUser component

--- front/src/components/pages/verify-user/VerifyUser.tsx
+++ front/src/components/pages/verify-user/VerifyUser.tsx
@@ -1,51 +1,33 @@
-import React, {FC, useEffect} from 'react';
-
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, {FC, useCallback, useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import {saveUserToken} from '../../../redux/reducers/auth-reducer/actions';
 import queryParams from '../../../helpers/libs/queryParams';
+import {useLocation} from "react-router-dom";
 
-interface IVerifyUser {
-  saveUserTokenFu: Function;
-  location: any;
-}
+interface IVerifyUser {}
 
-const VerifyUser: FC<IVerifyUser> = ({location, saveUserTokenFu}) => {
+const VerifyUser: FC<IVerifyUser> = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const saveUserTokenFu = useCallback(
+    (token: string) => {
+      dispatch(saveUserToken(token));
+    },
+    [dispatch]
+  );
+
   useEffect(() => {
     window.scrollTo(0, 0);
     const params = queryParams(location.search);
     const token = params['t'];
     if (token) {
-      console.log(token, 'tokenischeeeeeee');
-
       saveUserTokenFu(token);
-
-      // post('/api/find-user-by-token'
-      // TODO проверка этого токена!
-      // Если все хорошо даем редаксу понять что это норм токен
-      // Если плохо Выводим сообщение
-      // //
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
   return <div className='verifyUserContainer' />;
 };
 
-// const mapStateToProps = (state: any) => {
-//   return {
-//     authReducer: state.authReducer,
-//     isAdmin: state.isAdmin
-//   };
-// };
 
-const mapDispatchToProps = (dispatch: any) => {
-  return bindActionCreators(
-    {
-      saveUserTokenFu: (token) => saveUserToken(token)
-    },
-    dispatch
-  );
-};
 
-export default connect(null, mapDispatchToProps)(VerifyUser);
+
+export default VerifyUser;
