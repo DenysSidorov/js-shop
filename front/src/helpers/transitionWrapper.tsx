9ffbeadfdda14fb9b298888f6bb7546ffa9852ca
Denys Sidorov <000scorpions000@gmail.com>
2020-10-17 10:35:29 +0300

    add TransitionWrapper component for new animations

---
+++ front/src/helpers/transitionWrapper.tsx
@@ -0,0 +1,41 @@
+import React, {FC} from 'react';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+
+/**
+ * Migration from 1 to latest version
+ * https://github.com/reactjs/react-transition-group/blob/master/Migration.md
+ * */
+
+interface ITransitionWrapper {
+  children?: any;
+  transitionEnterTimeout: any;
+  transitionLeaveTimeout: any;
+  transitionAppearTimeout: any;
+  transitionAppear: any;
+}
+
+const TransitionWrapper = (props: ITransitionWrapper) => {
+  if (window.document && window.document.documentMode) {
+    return props.children || null;
+  }
+  if (props.children === null || props.children === false) {
+    return null;
+  }
+  return (
+    <TransitionGroup component='span'>
+      <CSSTransition
+        classNames='rdAppear'
+        timeout={{
+          enter: props.transitionEnterTimeout || props.time || 150,
+          exit: props.transitionLeaveTimeout || props.time || 150,
+          appear: props.transitionAppearTimeout || props.time || 150
+        }}
+        appear={props.transitionAppear !== undefined ? props.transitionAppear : true}
+      >
+        {props.children}
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
+export default TransitionWrapper;
