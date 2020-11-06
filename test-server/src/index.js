7ea078fd1cc0b53ddcc15702c27c1541ea01eaf2
Denys Sidorov <000scorpions000@gmail.com>
2020-11-06 22:57:21 +0200

    add envs to test docker file

--- test-server/src/index.js
+++ test-server/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 const app = express();
 
 app.get("/test", (req, res) => {
   res.send("Our api server is working correctly");
 });
 
-app.listen(3000, () => {
-  console.log("Started api service");
+app.listen(PORT, () => {
+  console.log("Started api service on port - ", PORT);
+  console.log("Main app's host is - ", HOST);
 });
