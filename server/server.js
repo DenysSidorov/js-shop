2a9d9d798ab61b5d6f042b3e1b05027293b5beff
Denys Sidorov <000scorpions000@gmail.com>
2020-11-10 23:47:37 +0200

    add some info, plans and logs

--- server/server.js
+++ server/server.js
@@ -48,8 +48,8 @@ app.use(require('prerender-node').set('prerenderToken', 'nVFIY5P2oHmWGlW1r6B3'))
 
 app.use(express.static(path.join(__dirname, '/www/'))); // static files
 app.use(morgan('tiny')); // logs
-app.use(bodyParse.json());
-app.use(bodyParse.urlencoded({extended: true}));
+app.use(bodyParse.json()); // parse json
+app.use(bodyParse.urlencoded({extended: true})); // parse forms
 app.set('view engine', 'ejs'); // template-engine by default
 
 app.use('/api/goods', cors(), goodRoute);
