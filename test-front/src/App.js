fe9669f63f7213d043e3fa95453abd4fefad371c
Denys Sidorov <000scorpions000@gmail.com>
2020-11-13 00:21:55 +0200

    add nginx.conf.dev and Dockerfile.dev, change docker-compose.configs

--- test-front/src/App.js
+++ test-front/src/App.js
@@ -15,7 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!!! Boo6
+          Edit <code>src/App.js</code> and save to reload!!!! Boo666
         </p>
         <a
           className="App-link"
