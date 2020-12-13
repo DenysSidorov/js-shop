42179488d30841fd59654ddc46c42434fe63d61b
Denys Sidorov <000scorpions000@gmail.com>
2020-12-14 00:49:01 +0200

    refactor orders/goods models in mongoose, v2

--- server/models/order.js
+++ server/models/order.js
@@ -1,23 +1,23 @@
 import mongoose, {Schema} from 'mongoose';
 
 const OrderSchema = new Schema({
-  payment: {type: Object, require: true, default: {}},
-  price: {type: Number, require: true, default: 0},
-  delivery: {type: String, require: true, default: '-'},
-  name: {type: String, require: true, default: '-'},
-  address: {type: String, require: true, default: '-'},
-  phone: {type: String, require: true},
+  payment: {type: Object, default: {}},
+  price: {type: Number, default: 0},
+  delivery: {type: String, default: '-'},
+  name: {type: String, default: '-'},
+  address: {type: String, default: '-'},
+  phone: {type: String, required: true},
   email: {type: String},
-  createdAt: {type: Date, require: true, default: Date.now},
-  type: {type: String, require: true, default: 'new'}, // TYPES: new, progress, done, delivery
+  createdAt: {type: Date, default: Date.now},
+  type: {type: String, default: 'new'}, // TYPES: new, progress, done, delivery
   goods: [
     {
-      _id: {type: String, require: true},
-      count: {type: Number, require: true},
-      name: {type: String, require: true},
-      model: {type: String, require: true},
-      sail: {type: Number, require: true},
-      price: {type: Number, require: true}
+      _id: {type: String},
+      count: {type: Number},
+      name: {type: String},
+      model: {type: String},
+      sail: {type: Number},
+      price: {type: Number}
     }
   ]
 });
