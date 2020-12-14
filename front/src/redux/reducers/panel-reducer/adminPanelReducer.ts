bc942c83b28ef98b665f23a02c132fc205612985
Denys Sidorov <000scorpions000@gmail.com>
2020-12-14 22:57:52 +0200

    rewrite adminPanelReducer with Immer.js

--- front/src/redux/reducers/panel-reducer/adminPanelReducer.ts
+++ front/src/redux/reducers/panel-reducer/adminPanelReducer.ts
@@ -1,4 +1,5 @@
 import {GET_TYPES} from './types';
+import {produce, Draft} from 'immer';
 
 export type AdminPanelStateType = {
   new: 0;
@@ -14,18 +15,22 @@ export interface IAdminPanel {
 
 const initialState: IAdminPanel = {countTypes: {new: 0, progress: 0, done: 0, delivery: 0}};
 
-export default function (state = initialState, action: any): IAdminPanel {
+const adminPanelReducer = produce((draft: Draft<IAdminPanel>, action: any): IAdminPanel | void => {
   switch (action.type) {
     case GET_TYPES: {
-      const newState: IAdminPanel = {...state, countTypes: {...initialState.countTypes}};
+
+      const nullState: AdminPanelStateType = {new: 0, progress: 0, done: 0, delivery: 0};
+
+      draft.countTypes = nullState;
+
       if (Array.isArray(action.payload)) {
         action.payload.forEach((el: any) => {
-          newState.countTypes[el._id] = el.count;
+          draft.countTypes[el._id] = el.count;
         });
       }
-      return newState;
     }
-    default:
-      return state;
   }
-}
+}, initialState);
+
+export default adminPanelReducer;
+
