9bc70d3107ae1a1e001bf4f62172c82d13e8ac6b
Denys Sidorov <000scorpions000@gmail.com>
2020-12-25 23:54:59 +0200

    remove last thunk's action creator from auth reducer part

--- front/src/redux/reducers/auth-reducer/sagas.ts
+++ front/src/redux/reducers/auth-reducer/sagas.ts
@@ -1,10 +1,9 @@
 import {takeLatest, all, put, call, fork} from 'redux-saga/effects';
 import {get} from 'lodash';
-import * as types from './types';
 import {hideLoading, showLoading} from 'react-redux-loading-bar';
 import {signIn, isAdmin, signUp} from '../../../api/endpoints';
 import {history} from '../../store/configureStore';
-import {APPEAR_LIKE_ADMIN, AUTH_USER, DISAPPEAR_LIKE_ADMIN, UNAUTH_USER} from './types';
+import * as types from './types';
 import {authError} from './actions';
 
 function* signInUserSaga(action: any) {
@@ -48,39 +47,18 @@ function* isAdminSaga() {
     const token = yield call([localStorage, localStorage.getItem], 'info');
     const response = yield call(isAdmin, token);
     if (get(response, 'data.isadmin', null)) {
-      yield put({type: APPEAR_LIKE_ADMIN});
-      yield put({type: AUTH_USER});
+      yield put({type: types.APPEAR_LIKE_ADMIN});
+      yield put({type: types.AUTH_USER});
 
     } else {
-      yield put({type: UNAUTH_USER});
-      yield put({type: DISAPPEAR_LIKE_ADMIN});
+      yield put({type: types.UNAUTH_USER});
+      yield put({type: types.DISAPPEAR_LIKE_ADMIN});
     }
   } catch (er) {
     console.log(er);
   }
 }
 
-// export function* isAdminSaga(token?: string | null) {
-//     try {
-//       if (!token) {
-//         // token = localStorage.getItem('info');
-//         yield call([localStorage, localStorage.getItem], 'info');
-//       }
-//       const isAdmin = await axios.post(`${urlApi}/api/isadmin`, {authtoken: token});
-//       console.log(isAdmin.data.isadmin, 'isamin');
-//
-//       if (isAdmin.data.isadmin) {
-//         dispatch({type: APPEAR_LIKE_ADMIN});
-//         dispatch({type: AUTH_USER});
-//       } else {
-//         dispatch({type: DISAPPEAR_LIKE_ADMIN});
-//       }
-//     } catch (er) {
-//       dispatch({type: UNAUTH_USER});
-//       dispatch({type: DISAPPEAR_LIKE_ADMIN});
-//       console.log(er);
-//     }
-// }
 
 export function* saveUserTokenSaga(action: any) {
   yield put({type: types.AUTH_USER});
