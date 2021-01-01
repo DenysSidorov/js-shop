3404231c7317a56ec1bf57b5ff5d43955f60db38
Denys Sidorov <000scorpions000@gmail.com>
2021-01-01 17:37:03 +0200

    remove unnecessary 'axios' imports

--- front/src/api/urlApi.ts
+++ front/src/api/urlApi.ts
@@ -1,10 +1,5 @@
 import config from '../config';
-import axios from './axiosConfig'
 
 const urlApi: string =
   config.NODE_ENV === 'development' ? `http://127.0.0.1:${config.API_PORT}` : window.location.origin;
 export default urlApi;
-
-setTimeout(()=>{
-  console.log(axios);
-}, 99999999);
