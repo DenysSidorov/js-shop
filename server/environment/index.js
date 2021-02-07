78d1b07dc5993cc5972ad8b3a9ed3829c91b3e5f
Denys Sidorov <000scorpions000@gmail.com>
2021-02-08 01:21:02 +0200

    improve ServerCreator's initial functional

--- server/environment/index.js
+++ server/environment/index.js
@@ -1,16 +1,7 @@
 import express from 'express';
-import config from '../config';
 import initGlobalMiddlewares from './globalMiddlewares';
 import initRoutes from './routes';
 import ServerCreator from './serverCreator';
-
-// import {serverApollo} from '../graphql/config';
-// console.log(serverApollo.graphqlPath);
-
-
-console.log('DEV MODE = ', config.NODE_ENV);
-
-
 
 export const application = () => {
   const app = express();
--- server/environment/serverCreator.js
+++ server/environment/serverCreator.js
@@ -2,21 +2,30 @@ import http from "http";
 import https from "https";
 import config from '../config';
 import fs from "fs";
+import {serverApollo} from '../graphql/config';
+
 
 export default class ServerCreator {
-  static up(){
+  static up(app){
 
-    const privateKey = fs.readFileSync('./sslcert/private.key', 'utf8');
-    const certificate = fs.readFileSync('./sslcert/certificate.crt', 'utf8');
-    const credentials = {key: privateKey, cert: certificate};
+    console.log('DEV MODE = ', config.NODE_ENV);
+    const isProd = config.NODE_ENV === 'production';
 
-
-    http.createServer(app);
-    https.createServer(credentials, app);
+    if (isProd) {
+      const privateKey = fs.readFileSync('./sslcert/private.key', 'utf8');
+      const certificate = fs.readFileSync('./sslcert/certificate.crt', 'utf8');
+      const credentials = {key: privateKey, cert: certificate};
+      https.createServer(credentials, app);
+    } else {
+      http.createServer(app);
+    }
 
     app.listen(config.PORT, (err) => {
       if (err) throw err;
-      console.log(`Server listening on port ${config.PORT}`);
+      console.log(
+        '🚀 Server ready at',
+        `${config.SERVER_DOMAIN}:${config.PORT}${serverApollo.graphqlPath}`
+      )
     });
   }
 }
