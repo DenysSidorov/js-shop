6d240938b4bbc98c16fef57b1c24eeb3f7cddc0e
Denys Sidorov <000scorpions000@gmail.com>
2021-02-12 00:30:30 +0200

    add typescript's configuration for Apollo client, v2

--- front/src/apollo.ts
+++ front/src/apollo.ts
@@ -1,8 +1,7 @@
-import ApolloClient from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-// import config from './config';
+const ApolloClient = require('apollo-boost').default;
+// // import config from './config';
 
-export const client = new ApolloClient<InMemoryCache>({
+export const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
   // uri: `${config.SERVER_DOMAIN}:${config.SERVER_PORT}/graphql`
 });
--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,36 +1,30 @@
 import {hot} from 'react-hot-loader/root';
 import React from 'react';
 import ReactDom from 'react-dom';
-import {ApolloProvider} from '@apollo/react-hooks';
 import {Provider} from 'react-redux';
 import {Router} from 'react-router-dom';
+import {ApolloProvider} from '@apollo/react-hooks';
 // import {BrowserRouter as Router} from 'react-router-dom';
-// import {client} from './apollo';
+import {client} from './apollo';
 import store, {history} from './redux/store/configureStore';
 import Wrapper from './components/index';
-import {ApolloClient, NormalizedCacheObject, ApolloCache} from '@apollo/client';
-// import WrapperTest from './components/test';
 
 const App = hot(Wrapper);
-
-export const client = new ApolloClient<{}>({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  // cache: ApolloCache
-  // uri: `${config.SERVER_DOMAIN}:${config.SERVER_PORT}/graphql`
-});
 
 const container = document.getElementById('main');
 if (container) {
   ReactDom.render(
-    <Provider store={store}>
-      <div>
-        <ApolloProvider client={client}>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <div>
           <Router history={history}>
             <App/>
           </Router>
-        </ApolloProvider>
-      </div>
-    </Provider>,
+        </div>
+      </Provider>
+    </ApolloProvider>
+    ,
+
     container
   );
 }
