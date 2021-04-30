13704ac5ceea5a591e32cdb1d13f94eccde4910d
Denys Sidorov <000scorpions000@gmail.com>
2021-04-30 23:50:25 +0300

    prepare application (front-panel) before global ts configuration

--- front-panel-v2/src/Heading.tsx
+++ front-panel-v2/src/Heading.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import heading from "./heading.module.css";
 
 export const Heading = () => (
-  <h1 className={heading.heading}>My React and TypeScript App</h1>
+  <h1 className={heading.heading}>My React and TypeScript App22</h1>
 );
